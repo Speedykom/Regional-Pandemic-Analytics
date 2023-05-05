37709b19287a9e8761b9c915356a7fe72ccf694e
ShekuMunu <munusheku@gmail.com>
2023-05-05 06:11:08 +0000

    run inotify in interactive mode

--- staging/hop/watchFiles.sh
+++ staging/hop/watchFiles.sh
@@ -5,9 +5,10 @@ WATCH_DIR="/home"
 
 echo "Running watcher..."
 
-inotifywait -m -d -o /files/watchRuns.txt -r "/home" | while read event; do
-
+# -d -o /files/watchRuns.txt
+inotifywait -m -r "/home" -e create | while read event; do
   # Run the chmod command on the modified file
   chmod -R 777 "${WATCH_DIR}"
   echo "Got a change" >> /files/watchRuns.txt
+  break
 done
