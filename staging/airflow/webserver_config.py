5cbc942f4a19df9d1477b95563b90a36ce80fed9
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-05-05 00:39:48 +0000

    update airflow

--- staging/airflow/webserver_config.py
+++ staging/airflow/webserver_config.py
@@ -21,10 +21,10 @@ PROVIDER_NAME = 'keycloak'
 CLIENT_ID = 'airflow'
 CLIENT_SECRET = 'CJZUXnQclzASQ98PzAVlsUNp2yZ3J3tf'
 
-OIDC_ISSUER = 'https://keycloak.speedykom.com/realms/regional-pandemic-analytics'
-KEYCLOAK_BASE_URL = 'https://keycloak.speedykom.com/realms/regional-pandemic-analytics/protocol/openid-connect'
-KEYCLOAK_TOKEN_URL = 'https://keycloak.speedykom.com/realms/regional-pandemic-analytics/protocol/openid-connect/token'
-KEYCLOAK_AUTH_URL = 'https://keycloak.speedykom.com/realms/regional-pandemic-analytics/protocol/openid-connect/auth'
+OIDC_ISSUER = 'http://auth2.igad-health.eu/realms/regional-pandemic-analytics'
+KEYCLOAK_BASE_URL = 'http://auth2.igad-health.eu/realms/regional-pandemic-analytics/protocol/openid-connect'
+KEYCLOAK_TOKEN_URL = 'http://auth2.igad-health.eu/realms/regional-pandemic-analytics/protocol/openid-connect/token'
+KEYCLOAK_AUTH_URL = 'http://auth2.igad-health.eu/realms/regional-pandemic-analytics/protocol/openid-connect/auth'
 
 AUTH_TYPE = AUTH_OAUTH
 AUTH_USER_REGISTRATION = True
