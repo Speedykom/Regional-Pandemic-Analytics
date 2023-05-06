0440714b8519dfb739df76283e049c6a4c3a2cbc
ShekuMunu <munusheku@gmail.com>
2023-05-06 09:21:18 +0000

    remove breaking of watcher

--- staging/hop/watchFiles.sh
+++ staging/hop/watchFiles.sh
@@ -10,5 +10,4 @@ inotifywait -m -r "/home" -e create | while read event; do
   # Run the chmod command on the modified file
   chmod -R 777 "${WATCH_DIR}"
   echo "Got a change" >> /files/watchRuns.txt
-  break
 done
