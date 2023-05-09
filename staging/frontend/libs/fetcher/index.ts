f707d28279dc61c20ebebceeedb9d86be32b3cd1
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-05-09 13:37:19 +0000

    setup frontend

---
+++ staging/frontend/libs/fetcher/index.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const axiosFetcher = async (
+  url: string,
+  username: string,
+  password: string
+) => {
+  const response = await axios.get(url, {
+    auth: {
+      username,
+      password,
+    },
+  });
+
+  return response.data;
+};
