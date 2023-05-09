a2bca67d6dc6d9c5e5043584c6cce8bc0eb3ab56
AhmAmri <ahmed.amri@speedykom.com>
2023-05-09 13:37:19 +0000

    setup frontend

---
+++ staging/frontend/components/Dashboard/DashboardFrame.tsx
@@ -0,0 +1,19 @@
+import Layout from "@/components/Dashboard/Layout";
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/router";
+
+
+export default function DashboardFrame(props){
+    const sessionData = useSession()
+    const router = useRouter()
+    if(sessionData?.status == 'unauthenticated'){
+        router.push("/")
+        return null
+    }
+    return(
+        <Layout>
+            <p className="text-gray-700 text-3xl mb-8 font-bold">{props.title}</p>
+            {props.children}
+        </Layout>
+    )
+}
