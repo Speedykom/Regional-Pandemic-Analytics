7ef8c1d304da07a9ae11a23990d6dd647d4cc3ad
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-05-09 14:35:58 +0000

    Update Dashboard.tsx

--- staging/frontend/components/TABS/Dashboard.tsx
+++ staging/frontend/components/TABS/Dashboard.tsx
@@ -34,7 +34,7 @@ export default function Dashboard() {
             await embedDashboard({
                 id: '5e676cb1-7330-4eed-9e1a-d432de97da5e',
                 supersetDomain: 'http://localhost:8080',
-                mountPoint: document.getElementById('igad-covid-dashboard'),
+                mountPoint: document.getElementById('igad-covid-dashboard') || null,
                 fetchGuestToken: () => getGuestToken(),
                 dashboardUiConfig: {
                     hideTitle: true,
