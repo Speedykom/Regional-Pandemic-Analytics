587b6170cca055967b8836ebbe8054f8fbb75926
AhmAmri <ahmed.amri@speedykom.com>
2023-05-10 03:45:19 +0000

    complete dynamic dag backed

--- staging/backend/airflow/app.py
+++ staging/backend/airflow/app.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DDagsConfig(AppConfig):
+class AirflowConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ddags'
+    name = 'airflow'
