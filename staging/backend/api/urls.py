f80bd2ae4a5275cf34c8ace49494af3437a98620
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-05-11 04:40:54 +0000

    Feature: Add Authentication API for backend

--- staging/backend/api/urls.py
+++ staging/backend/api/urls.py
@@ -3,7 +3,7 @@ from .import views
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.urls import re_path
+from django.urls import re_path, path
 
 app_name = 'api'
 
@@ -25,4 +25,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Login Endpoint
+    path('auth/', views.LoginAPI.as_view()),
+
 ]
