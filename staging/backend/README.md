915e9a56d626b2e67b469b5894051f1741296293
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-05-11 04:40:54 +0000

    Feature: Add Authentication API for backend

--- staging/backend/README.md
+++ staging/backend/README.md
@@ -1,1 +1,2 @@
 ## IGAD Application Backend
+
--- staging/backend/core/urls.py
+++ staging/backend/core/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('accounts.urls')),
-    path('health/', include('health.urls')),
-    path('airflow/', include('airflow.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
