3eab24032bcfec0be63165da1eb568fa92866309
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-05-11 05:23:53 +0000

    Feature: Create Authentication API for Backend and Add components and API for viewing superset dashboard dynamically

--- staging/backend/airflow/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import DagView
-
-urlpatterns = [
-    path('airflow/', DagView.as_view()),
-    path('airflow/<str:id>/', DagView.as_view()),  
-    path('airflow/<str:id>/update/', DagView.as_view())  
-]
