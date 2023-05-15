99f9ae9123358bc700d51b67f55ff89f868d9f5a
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-05-15 22:28:20 +0000

    Fix: Bug in dashboardUUID Component

--- staging/frontend/pages/dashboard/superset-dashboards/[dashboardUUID].tsx
+++ staging/frontend/pages/dashboard/superset-dashboards/[dashboardUUID].tsx
@@ -3,13 +3,19 @@ import {useEffect} from "react";
 import {embedDashboard} from "@superset-ui/embedded-sdk";
 import {useRouter} from "next/router";
 
+interface RouterQuery {
+    dashboardUUID?: string;
+    dashboardTitle?: string;
+}
+
+
 export default function SupersetDashboard(){
     const router = useRouter()
 
-    const {dashboardUUID, dashboardTitle} = router.query
+    const { dashboardUUID, dashboardTitle } = router.query as RouterQuery;
 
     const getGuestToken = async () => {
-        const queryParams = new URLSearchParams({dashboardUUID})
+        const queryParams = new URLSearchParams({dashboardUUID: String(dashboardUUID)})
         const response = await fetch(`/api/get-guest-token/?${queryParams}`)
         const token = await response.json()
         return token?.guestToken
@@ -17,24 +23,27 @@ export default function SupersetDashboard(){
 
     useEffect(() => {
         const embed = async () => {
-            await embedDashboard({
-                id: `${dashboardUUID}`,
-                supersetDomain: `${process.env.NEXT_PUBLIC_SUPERSET_URL}`,
-                mountPoint: document.getElementById('igad-covid-dashboard'),
-                fetchGuestToken: () => getGuestToken(),
-                dashboardUiConfig: {
-                    hideTitle: true,
-                    hideChartControls: true,
-                    hideTab: true,
-                },
-            });
+            const mountPoint = document.getElementById('igad-covid-dashboard');
+            if(mountPoint){
+                await embedDashboard({
+                    id: `${dashboardUUID}`,
+                    supersetDomain: `${process.env.NEXT_PUBLIC_SUPERSET_URL}`,
+                    mountPoint,
+                    fetchGuestToken: () => getGuestToken(),
+                    dashboardUiConfig: {
+                        hideTitle: true,
+                        hideChartControls: true,
+                        hideTab: true,
+                    },
+                });
+            }
         };
         if (document.getElementById('igad-covid-dashboard')) {
             embed();
         }
     }, []);
     return(
-        <DashboardFrame title={dashboardTitle}>
+        <DashboardFrame title={dashboardTitle || ''}>
             <div id="igad-covid-dashboard" />
         </DashboardFrame>
     )
