fe26ca6116729ff6d2d1a6e42f7b5820562b3846
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-05-17 06:17:35 +0000

    keycloak auth

--- staging/backend/core/settings.py
+++ staging/backend/core/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django-keycloak-auth.middleware.KeycloakMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
