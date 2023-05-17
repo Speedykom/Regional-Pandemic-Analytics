c844fb66cc7b24786577b162f2e22ce5d09543e8
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-05-17 06:31:35 +0000

    Feature: Add Keycloak Login API and integrate with the Frontend

--- staging/frontend/pages/index.tsx
+++ staging/frontend/pages/index.tsx
@@ -1,29 +1,143 @@
-import Head from 'next/head';
-import { Inter } from 'next/font/google';
-import { useState, useEffect } from 'react';
-import Layout from "@/components/Dashboard/Layout";
-import AnalyticsCard from "@/components/Dashboard/AnalyticsCard";
-import TimeSeries from "@/components/Dashboard/TimeSeries";
-import DashboardFrame from "@/components/Dashboard/DashboardFrame";
+import Head from "next/head";
+import Image from "next/image";
+import css from "styled-jsx/css";
+import {useForm} from "react-hook-form";
+import * as z from 'zod'
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useRouter} from "next/router";
+import axios from "axios";
 
-const inter = Inter({ subsets: ['latin'] });
+const schema = z.object({
+    username: z
+        .string()
+        .min(5, { message: 'Username must contain at least 5 character(s)' }),
+    password: z
+        .string()
+        .min(5, { message: 'Password must contain at least 5 character(s)' }),
+});
 
-export default function Home() {
-    return (
+
+type TFormValues = {
+    username: string;
+    password: string;
+};
+
+type TPayload = {
+    [key: string]: string;
+};
+
+const globalStyles = css.global`
+  body {
+    background-color: #eeeeee;
+  }
+`;
+
+export default function LoginForm(){
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<TFormValues>({ resolver: zodResolver(schema) });
+
+    const handleLogin = (data: TFormValues) => {
+        axios
+            .post('/api/accounts/login/', JSON.stringify(data), {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((response) => {
+               console.log(response.data)
+            })
+            .catch((error: unknown) => {
+                if (error instanceof Error)
+                    console.log(error);
+            });
+    };
+    return(
         <>
             <Head>
-                <title>Dashboard | RePAN</title>
-                <meta name="description" content="SpeedyKom" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Regional Pandemic Analytics Tool | Welcome</title>
             </Head>
-            <DashboardFrame title="Dashboard">
-                <div className="">
-                    <AnalyticsCard/>
+            <style jsx>{globalStyles}</style>
+            <section className="gradient-form md:h-screen">
+                <div className="container mx-auto px-6" style={{marginTop:"10%"}}>
+                    <div className="flex justify-center items-center flex-wrap g-6 text-gray-800">
+                        <div className="xl:w-10/12">
+                            <div className="block bg-white shadow-lg rounded-lg">
+                                <div className="lg:flex lg:flex-wrap g-0">
+                                    <div className="lg:w-6/12 px-4 md:px-0">
+                                        <div className="md:p-12 md:mx-6">
+                                            <div className="text-center">
+                                                <Image
+                                                    className="mx-auto w-48"
+                                                    src="/images/igad_logo.jpg"
+                                                    alt="logo"
+                                                    width={450}
+                                                    height={300}
+                                                />
+                                            </div>
+                                            <form onSubmit={handleSubmit(handleLogin)}>
+                                                <p className="mt-3 mb-4 text-center font-semibold">
+                                                    Please login to your account
+                                                </p>
+                                                <div className="mb-4">
+                                                    <input
+                                                        {...register('username')}
+                                                        id="username"
+                                                        name="username"
+                                                        type="username"
+                                                        autoComplete="username"
+                                                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                                        placeholder="Username"
+                                                    />
+                                                    <span className="text-xs tracking-wide text-red-600">
+                                                        {errors?.username && errors.username.message}
+                                                    </span>
+                                                </div>
+                                                <div className="mb-4">
+                                                    <input
+                                                        {...register('password')}
+                                                        id="password"
+                                                        name="password"
+                                                        type="password"
+                                                        autoComplete="current-password"
+                                                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                                    />
+                                                    <span className="text-xs tracking-wide text-red-600">
+                                                        {errors?.password && errors.password.message}
+                                                    </span>
+                                                </div>
+                                                <button
+                                                    className="bg-emerald-700 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-emerald-500 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+
+                                                    data-mdb-ripple="true"
+                                                    data-mdb-ripple-color="light"
+                                                    onClick={()=> console.log("clicked")}
+                                                >
+                                                    Log in
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <div className="bg-emerald-700 lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none">
+                                        <div className="text-white px-4 py-6 md:p-12 md:mx-6">
+                                            <h4 className="text-2xl font-semibold mb-4">
+                                                Welcome back!
+                                            </h4>
+                                            <p className="text-sm">
+                                                Simply login to access the IGAD regional pandemic analytics tool to collect, analyze,
+                                                and report granular and aggregated data from multiple sources for informed decision-making.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="mt-3 mb-3">
-                    <TimeSeries/>
-                </div>
-            </DashboardFrame>
+            </section>
         </>
-    );
-}
+    )
+} 
