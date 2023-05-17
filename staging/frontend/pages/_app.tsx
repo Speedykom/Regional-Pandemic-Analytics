7e4765ef518b1a1b5c310ba2fe188a9c1ffabe19
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-05-17 06:31:35 +0000

    Feature: Add Keycloak Login API and integrate with the Frontend

--- staging/frontend/pages/_app.tsx
+++ staging/frontend/pages/_app.tsx
@@ -1,23 +1,20 @@
-import "@/styles/globals.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import type { AppProps } from "next/app";
-import { AuthProvider } from "@/context/auth";
-import { useAuth } from "@/context/auth";
-import { PageLoad } from "@/components/Loader";
+import {ToastContainer} from "react-toastify";
+import {SWRConfig} from "swr";
+import axios from "axios";
+import {AppProps} from "next/app";
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  const { isLoading } = useAuth();
+export default function App({ Component, pageProps}:AppProps) {
   return (
-    <AuthProvider>
-      {isLoading ? (
-        <PageLoad />
-      ) : (
-        <>
-          <Component {...pageProps} />
-          <ToastContainer />
-        </>
-      )}
-    </AuthProvider>
-  );
+      <SWRConfig
+          value={{
+            refreshInterval: 3000,
+            fetcher: (resource, init) =>
+                axios.get(resource, init).then((res) => res.data),
+          }}
+      >
+        <Component {...pageProps} />
+        <ToastContainer/>
+      </SWRConfig>
+  )
 }
