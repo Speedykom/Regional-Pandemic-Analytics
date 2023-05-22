8a6c59887040db2d8a0c29e7068746549bc93569
Mohamed James Kamara <intern_mohamed.kamara@gtbank.com>
2023-05-22 04:54:45 +0000

    remove unused imports

--- staging/frontend/pages/users/index.tsx
+++ staging/frontend/pages/users/index.tsx
@@ -1,10 +1,5 @@
 import DashboardFrame from "@/components/Dashboard/DashboardFrame";
-import { IGADTable } from "@/components/common/table";
-import { useUsers } from "@/src/module/user/hooks";
-import { IUser } from "@/src/module/user/interface";
 import { UserList } from "@/src/module/user/views/List";
-import { PlusIcon } from "@heroicons/react/24/outline";
-import { Button, Menu } from "antd";
 
 export const LoadUsers = () => {
 	return (
