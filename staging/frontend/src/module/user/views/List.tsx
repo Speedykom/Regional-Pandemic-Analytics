090a1c254d418c46773d670bff583358db374651
Mohamed James Kamara <intern_mohamed.kamara@gtbank.com>
2023-05-22 04:50:44 +0000

    features: forgot password button, reset password page, user view, user create button, user table hook, common components

---
+++ staging/frontend/src/module/user/views/List.tsx
@@ -0,0 +1,49 @@
+import { IGADTable } from "@/components/common/table"
+import { PlusCircleOutlined, PlusOutlined } from "@ant-design/icons"
+import { Button } from "antd"
+import { useUsers } from "../hooks";
+import { IUser } from "../interface";
+
+export const UserList = () => {
+    const { rows, columns, loading } = useUsers();
+    return (
+        <div className="">
+				<nav>
+					<div className="flex justify-between">
+						<div>
+							<h2 className="text-3xl">App Accounts</h2>
+							<p className="my-2 text-gray-600">
+								View and manage settings related to app users.
+							</p>
+						</div>
+						<div>
+							<Button
+                            type="primary"
+                            className="flex items-center"
+								icon={<PlusOutlined />}
+								style={{
+									backgroundColor: "#e65e01",
+                                    border: "1px solid #e65e01",
+								}}
+							>
+								New User
+							</Button>
+						</div>
+					</div>
+				</nav>
+				<section className="mt-5">
+					<div className="py-2">
+						<IGADTable
+							key={"id"}
+							loading={loading}
+							rows={rows}
+							onRow={(record: IUser) => ({
+								onClick: () => alert(`Hello, ${record.firstName}`),
+							})}
+							columns={columns}
+						/>
+					</div>
+				</section>
+			</div>
+    )
+}
