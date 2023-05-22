0f00b0b1e95dea628f292b522770e7a875474690
Mohamed James Kamara <intern_mohamed.kamara@gtbank.com>
2023-05-22 07:26:57 +0000

    features: user actions in table, drawer to create users

--- staging/frontend/pages/users/index.tsx
+++ staging/frontend/pages/users/index.tsx
@@ -1,7 +1,9 @@
 import DashboardFrame from "@/components/Dashboard/DashboardFrame";
 import { UserList } from "@/src/module/user/views/List";
+import { useState } from "react";
 
 export const LoadUsers = () => {
+    
 	return (
 		<DashboardFrame title="List(s) of Users">
 			<UserList />
