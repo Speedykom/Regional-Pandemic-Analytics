6540b7b2bb31fcd8e18f2cfe214fd61670c46743
AhmAmri <ahmed.amri@speedykom.com>
2023-05-23 04:37:02 +0000

    process-chain

--- staging/backend/airflow/views/dags.py
+++ staging/backend/airflow/views/dags.py
@@ -4,21 +4,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 api = os.getenv("AIRFLOW_API")
-
+username = os.getenv("AIRFLOW_USER")
+password = os.getenv("AIRFLOW_PASSWORD")
 
 class DagApiView(APIView):
 
     def get(self, request, id=None):
-        print(api)
-
         if id:
             route = "{}/dags/{}".format(api, id)
             client = requests.get(route)
-            print(client)
-            return Response({'status': 'success', "dag": {}}, status=200)
+            return Response({'status': 'success', "dag": client.json()}, status=200)
 
         route = "{}/dags".format(api)
-        print(route)
-        client = requests.get(route)
-        print(client)
-        return Response({'status': 'success', "dags": []}, status=200)
+        client = requests.get(route, auth=(username, password))
+        return Response({'status': 'success', "dags": client.json()['dags']}, status=200)
