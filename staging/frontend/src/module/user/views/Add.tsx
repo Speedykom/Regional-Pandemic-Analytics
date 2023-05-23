96d55bc059e2f7e8b75aceb53ea92e0d4d8bc9f0
Mohamed James Kamara <intern_mohamed.kamara@gtbank.com>
2023-05-23 06:26:42 +0000

    create user form design

--- staging/frontend/src/module/user/views/Add.tsx
+++ staging/frontend/src/module/user/views/Add.tsx
@@ -1,16 +1,78 @@
 import { SaveOutlined } from "@ant-design/icons";
-import { Button, Drawer } from "antd";
+import {
+	AutoComplete,
+	Button,
+	Cascader,
+	Checkbox,
+	Col,
+	Drawer,
+	Form,
+	Input,
+	InputNumber,
+	Row,
+	Select,
+	Switch,
+} from "antd";
+import { useState } from "react";
 
 interface props {
 	openDrawer: boolean;
 	closeDrawer: () => void;
 }
 
+const OPTIONS = ['Apples', 'Nails', 'Bananas', 'Helicopters'];
+
 export const AddUser = ({ openDrawer, closeDrawer }: props) => {
+	const [selectedItems, setSelectedItems] = useState<string[]>([]);
+	const [enabled, setEnabled] = useState(false);
+
+  const filteredOptions = OPTIONS.filter((o) => !selectedItems.includes(o));
+
+	const [form] = Form.useForm();
+
+	const onFinish = (values: any) => {
+		console.log("Received values of form: ", values);
+	};
+
+	const formItemLayout = {
+		labelCol: {
+			xs: { span: 24 },
+			sm: { span: 8 },
+		},
+		wrapperCol: {
+			xs: { span: 24 },
+			sm: { span: 16 },
+		},
+	};
+
+	const tailFormItemLayout = {
+		wrapperCol: {
+			xs: {
+				span: 24,
+				offset: 0,
+			},
+			sm: {
+				span: 16,
+				offset: 8,
+			},
+		},
+	};
+
+	const triggerEnabled = () => {
+		if (enabled) {
+			setEnabled(false)
+		}
+		else {
+			setEnabled(true)
+		}
+		console.log({enabled});
+		
+	}
+
 	return (
 		<Drawer
-            title={"Create a user"}
-            size={"large"}
+			title={"Create a user"}
+			size={"large"}
 			placement={"right"}
 			closable={true}
 			className="border-2"
@@ -29,18 +91,114 @@ export const AddUser = ({ openDrawer, closeDrawer }: props) => {
 						Cancel
 					</button>
 					<Button
-							type="primary"
-							className="flex items-center"
-							icon={<SaveOutlined />}
-							style={{
-								backgroundColor: "#087757",
-								border: "1px solid #e65e01",
-                            }}
-						>
-							New User
-						</Button>
+						type="primary"
+						className="flex items-center"
+						icon={<SaveOutlined />}
+						style={{
+							backgroundColor: "#087757",
+							border: "1px solid #e65e01",
+						}}
+					>
+						New User
+					</Button>
 				</div>
 			}
-		></Drawer>
+		>
+			<Form
+				{...formItemLayout}
+				form={form}
+				name="register"
+				onFinish={onFinish}
+				style={{ maxWidth: 600 }}
+				scrollToFirstError
+			>
+				<Form.Item
+					name="firstName"
+					label="Given Names"
+					rules={[
+						{
+							required: true,
+							message: "Please input your given names",
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					name="lastName"
+					label="Family Name"
+					rules={[
+						{
+							required: true,
+							message: "Please input your family name",
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					name="email"
+					label="E-mail"
+					rules={[
+						{
+							type: "email",
+							message: "The input is not valid E-mail!",
+						},
+						{
+							required: true,
+							message: "Please input your E-mail!",
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					name="username"
+					label="Username"
+					rules={[
+						{
+							required: true,
+							message: "Please input your username",
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+
+				<Form.Item
+					name="enabled"
+					label="Enable"
+					valuePropName="checked"
+					tooltip="Do you want to automatically enable this user?"
+				>
+					<Switch checked={enabled} onChange={triggerEnabled} style={{ backgroundColor: "#8c8c8c" }} />
+				</Form.Item>
+
+				<Form.Item
+					name="realmRoles"
+					label="Assign Roles"
+					tooltip="Select roles to assign to the user"
+				>
+					<Select
+						mode="multiple"
+						placeholder="Inserted are removed"
+						value={selectedItems}
+						onChange={setSelectedItems}
+						className="w-full"
+						style={{ width: "100%" }}
+						options={filteredOptions.map((item) => ({
+							value: item,
+							label: item,
+						}))}
+					/>
+				</Form.Item>
+				
+				<Form.Item {...tailFormItemLayout}>
+					<Button type="primary" htmlType="submit">
+						Register
+					</Button>
+				</Form.Item>
+			</Form>
+		</Drawer>
 	);
 };
