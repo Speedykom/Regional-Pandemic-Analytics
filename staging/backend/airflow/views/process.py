71c8842b84c20924ecdb2f7f21ba9290d523aba0
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-05-23 06:21:35 +0000

    query process chain

--- staging/backend/airflow/views/process.py
+++ staging/backend/airflow/views/process.py
@@ -7,14 +7,19 @@ api = os.getenv("AIRFLOW_API")
 username = os.getenv("AIRFLOW_USER")
 password = os.getenv("AIRFLOW_PASSWORD")
 
-class DagApiView(APIView):
-
+class GetProcessChain(APIView):
     def get(self, request, id=None):
         if id:
             route = "{}/dags/{}".format(api, id)
-            client = requests.get(route)
+            client = requests.get(route, auth=(username, password))
             return Response({'status': 'success', "dag": client.json()}, status=200)
 
         route = "{}/dags".format(api)
         client = requests.get(route, auth=(username, password))
         return Response({'status': 'success', "dags": client.json()['dags']}, status=200)
+
+class RunProcessChain(APIView):
+    def post(self, request, id=None):
+        route = "{}/dags/{}/dagRuns".format(api, id)
+        client = requests.post(route, json={}, auth=(username, password))
+        return Response({'status': 'success', "message": "{} process start running!".format(id)}, status=200)
