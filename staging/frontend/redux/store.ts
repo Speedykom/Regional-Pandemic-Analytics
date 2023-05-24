872636e2ea0ea522c21a894016db967ebd3faee9
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-05-24 06:57:21 +0000

    show dynamic dag

---
+++ staging/frontend/redux/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { processApi } from "./services/process";
+
+export const store = configureStore({
+  reducer: {
+    [processApi.reducerPath]: processApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(processApi.middleware),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
