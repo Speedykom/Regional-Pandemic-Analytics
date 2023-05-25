7cc954d94abde5a1843efeb016131b6530a95df3
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-05-25 09:28:28 +0000

    Feature: Add API and component for getting and displaying the list of uploaded files

--- staging/backend/data/serializers.py
+++ staging/backend/data/serializers.py
@@ -6,3 +6,8 @@ class FileUploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileUpload
         fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super(FileUploadSerializer, self).to_representation(instance)
+        representation['date_added'] = instance.date_added.strftime("%d %B, %Y")
+        return representation
