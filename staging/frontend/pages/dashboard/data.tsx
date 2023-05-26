81999957e70d6b9a8a97e59c816cce3f380bbe13
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-05-26 09:24:54 +0000

    Feature: Add support for multiple file upload and update upload API

--- staging/frontend/pages/dashboard/data.tsx
+++ staging/frontend/pages/dashboard/data.tsx
@@ -3,15 +3,29 @@ import ListData from "@/components/Data/ListData";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import {IData} from "@/components/Data/ListData";
+import secureLocalStorage from "react-secure-storage";
 
 export default function DataPage() {
     const [data, setData] = useState<IData[]>([]); // Specify the type as an array of IData objects
+
+    const [username, setUsername] = useState<string>("")
+
+
+    useEffect(() => {
+        if(typeof window !== undefined && window.localStorage){
+            setUsername(secureLocalStorage.getItem("sue") as string)
+        }
+    }, [])
 
     useEffect(() => {
         const fetchUploads = async () => {
             try {
                 const url = "/api/data/get-list-of-uploads/";
-                const response = await axios.get<IData[]>(url);
+                const response = await axios.get<IData[]>(url, {
+                    params:{
+                        username,
+                    }
+                });
                 // @ts-ignore
                 setData(response?.data?.result);
             } catch (error) {
@@ -19,7 +33,7 @@ export default function DataPage() {
             }
         };
         fetchUploads();
-    }, []);
+    }, [username]);
 
     return (
         <DashboardFrame title="List(s) of Uploaded files">
