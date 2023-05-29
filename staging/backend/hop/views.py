cfc4ec33966c0bf7134305b3d6d223aa1940c91f
maej <muhammadsesay8@gmail.com>
2023-05-29 05:34:14 +0000

    Add" : hop files directory in the settings Add: a method to get all the filename from the directory

--- staging/backend/hop/views.py
+++ staging/backend/hop/views.py
@@ -1,6 +1,9 @@
+import os
+from typing import List
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from core import settings
 
 # Create your views here.
 class ListHopViewSet(ViewSet):
@@ -8,10 +11,22 @@ class ListHopViewSet(ViewSet):
     This view returns the api response for the hop
     """
 
-    # 
+    def get_all_directory_files(self)-> List[str]:
+      """Looks into the hops template, iterate over the files, append and return"""
+      files: List[str] = []
+    
+      for filename in os.listdir(settings.HOP_FILES_DIR):
+        if filename.endswith(".hpl"):
+          files.append(filename)
+        else:
+          continue
+      
+      return files
+
   
     def get_all(self, request):
-      return Response({'status': 'success', "message": "listing hops"}, status=200)
+          #   file_path = os.path.join(settings.HOP_FILES_DIR, 'test.txt')
+      return Response({'status': 'success', "message": "{}".format(self.get_all_directory_files())}, status=200)
     
     def get_single(self, request, hop_title):
       # request.data.get("name", None),
