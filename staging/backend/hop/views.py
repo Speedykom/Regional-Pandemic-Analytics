8c0cd808659de1c3bc0c4dc17d015940024d1434
maej <muhammadsesay8@gmail.com>
2023-05-29 07:42:53 +0000

    Refactor: views from viewset to apiview Update method to get single hop

--- staging/backend/hop/views.py
+++ staging/backend/hop/views.py
@@ -1,12 +1,13 @@
 import os
 from typing import List
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from core import settings
+from bs4 import BeautifulSoup
+from django.http import HttpResponse
 
 # Create your views here.
-class ListHopViewSet(ViewSet):
+class ListHopAPIView(APIView):
     """
     This view returns the api response for the hop
     """
@@ -23,11 +24,32 @@ class ListHopViewSet(ViewSet):
       
       return files
 
+    def get(self, request):
+      """Returns all the files from the hop directory"""
+      return Response({'status': 'success', "data": "{}".format(self.get_all_directory_files())}, status=200)
   
-    def get_all(self, request):
-          #   file_path = os.path.join(settings.HOP_FILES_DIR, 'test.txt')
-      return Response({'status': 'success', "message": "{}".format(self.get_all_directory_files())}, status=200)
-    
-    def get_single(self, request, hop_title):
+      
+class GetSingleHopAPIView(APIView):
+    """
+    Returns a single hop data in xml format
+    """
+
+    def get_file_by_name(self, filename: str)-> any:
+      """Looks for a file by it name and return the found item"""
+      if os.path.isfile(os.path.join(settings.HOP_FILES_DIR, filename)):
+        return os.path.join(settings.HOP_FILES_DIR, filename)
+      else:
+        return False
+  
+    def get(self, request, filename: str):
+      """Returns a single file"""
+      result = self.get_file_by_name(filename)
+      if result:
+        contents = None
+        with open(result) as f:
+          contents = f.read()
+        bs_data = BeautifulSoup(contents, "xml")
+        return HttpResponse(bs_data.prettify(), content_type="text/xml")
+      else:
+        return Response({'status': 'error', "message": "No match found! No filename match: {}".format(filename)}, status=404)
       # request.data.get("name", None),
-      return Response({'status': 'success', "message": "listing single hop {}".format(hop_title)}, status=200)
