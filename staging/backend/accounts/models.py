7213184b68c54d0be52a9502e8891e8a533505f3
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-05 10:54:07 +0000

    fix children props error

--- staging/backend/accounts/models.py
+++ staging/backend/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django_minio_backend import MinioBackend, iso_date_prefix
 
 
-class AvatarUpload(models.Model):
-    type = models.CharField(max_length=5)
-    size = models.BigIntegerField()
-    file = models.FileField(storage=MinioBackend(bucket_name='avatars'), upload_to=iso_date_prefix)
+# class AvatarUpload(models.Model):
+#     type = models.CharField(max_length=5)
+#     size = models.BigIntegerField()
+#     file = models.FileField(storage=MinioBackend(bucket_name='avatars'), upload_to=iso_date_prefix)
