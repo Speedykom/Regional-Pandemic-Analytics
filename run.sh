d8d7cd80c1c9102bfc75c4581cfce85e6044eef6
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-06 03:04:18 +0000

    complete ci-cd

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker-compose down --remove-orphans
 docker-compose build && clear
-docker-compose -f docker-compose-staging.yml --env-file .env.staging up -d
+docker-compose up -d
 
 echo "Running!"
