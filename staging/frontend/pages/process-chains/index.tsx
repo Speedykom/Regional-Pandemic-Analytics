71ac079727b8039b55568684eafcacb21b04d56d
maej <contact@maej.dev>
2023-06-07 08:53:11 +0000

    Add: logic to hide and show the process chain and hop template modals

--- staging/frontend/pages/process-chains/index.tsx
+++ staging/frontend/pages/process-chains/index.tsx
@@ -12,6 +12,7 @@ import { TextInput } from "@tremor/react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import Router from "next/router";
+import { Select, Space } from "antd";
 
 type FormValues = {
   dagName: string;
@@ -24,6 +25,7 @@ type FormValues = {
 
 export default function ProcessChains() {
   const [open, setOpen] = useState(false);
+  const [openHopModal, setOpenHopModal] = useState(false);
 
   const {
     register,
@@ -51,11 +53,22 @@ export default function ProcessChains() {
     setOpen(false);
   });
 
-  const handleBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setOpen(!open);
+  const handleBtnClick = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    modalType: string
+  ) => {
+    if (modalType === "gotoProcessChain") {
+      setOpenHopModal(false);
+      setOpen(!open);
+    }
+    if (modalType === "gotoHopTemplate") {
+      setOpen(false);
+      setOpenHopModal(!openHopModal);
+    }
   };
   return (
     <DashboardFrame title="List(s) of Process Chains">
+      {/* process chain modal */}
       <Transition.Root show={open} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={setOpen}>
           <Transition.Child
@@ -143,7 +156,27 @@ export default function ProcessChains() {
                         </div>
                       </div>
                     </div>
-                    <div className="mt-5 sm:mt-6">
+                    <div className="mt-5 sm:mt-6 flex space-x-2">
+                      <button
+                        type="button"
+                        onClick={(e) => handleBtnClick(e, "gotoProcessChain")}
+                        className="inline-flex w-10 justify-center rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
                       <button
                         type="submit"
                         className="inline-flex w-full justify-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
@@ -158,13 +191,91 @@ export default function ProcessChains() {
           </div>
         </Dialog>
       </Transition.Root>
+
+      {/* add/create new hop template */}
+      <Transition.Root show={openHopModal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpenHopModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                  <form>
+                    <div>
+                      <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                        <AdjustmentsHorizontalIcon
+                          className="h-6 w-6 text-green-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="mt-3 text-center sm:mt-5">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          Hop Template
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <div>
+                            <Select
+                              placeholder="Please select an hop template"
+                              className="w-full"
+                              onChange={(value) => console.log(value)}
+                              options={[
+                                { value: "jack", label: "Jack" },
+                                { value: "lucy", label: "Lucy" },
+                                { value: "Yiminghe", label: "yiminghe" },
+                              ]}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    {/* button section */}
+                    <div className="mt-5 sm:mt-6">
+                      <button
+                        type="button"
+                        onClick={(e) => handleBtnClick(e, "gotoProcessChain")}
+                        className="inline-flex w-full justify-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                      >
+                        Continue
+                      </button>
+                    </div>
+                  </form>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+      {/* trigger button */}
       <button
-        onClick={handleBtnClick}
+        onClick={(e) => handleBtnClick(e, "gotoHopTemplate")}
         className="px-3 py-1 border border-blue-500 text-blue-500 rounded-md hover:bg-blue-500 hover:text-white focus:outline-none focus:bg-blue-500 focus:text-white"
       >
         Add Process Chain
       </button>
-      <Dag/>
+      <Dag />
     </DashboardFrame>
   );
 }
