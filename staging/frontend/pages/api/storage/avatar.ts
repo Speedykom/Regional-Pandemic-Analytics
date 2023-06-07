ce23fee601a7d6f844c617114b847444c2a1bece
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-07 09:38:19 +0000

    user manager

--- staging/frontend/pages/api/storage/avatar.ts
+++ staging/frontend/pages/api/storage/avatar.ts
@@ -1,13 +1,26 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export const getAvatar = async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { url } = req.query;
     return await axios.get(`${url}`).then((data) => {
-        return res.status(200).send(data.data)
+        const stream: any = data;
+        const contentType = stream.headers['content-type'];
+
+        res.writeHead(200, 'OK', {
+            'Content-Type': 'binary/octet-stream',
+            'Transfer-Encoding': 'chunked'
+        })
+        stream.on('data', function (chunk: any) {
+            res.write(chunk);
+        })
+        stream.on('end', function () {
+            res.end();
+        })
+        stream.on('error', function (err: any) {
+            console.log(err)
+        })
     }).catch(() => {
         return res.send("")
     })
 }
-
-export default getAvatar;
