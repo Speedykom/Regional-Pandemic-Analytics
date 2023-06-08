3b2cfdbf9a7ae621f90465e46ef0619c33f2e96f
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-08 09:39:16 +0000

    superset auth access

--- staging/frontend/pages/users/index.tsx
+++ staging/frontend/pages/users/index.tsx
@@ -1,18 +1,11 @@
 import DashboardFrame from "@/components/Dashboard/DashboardFrame";
 import { UserList } from "@/src/module/user/views/List";
-import { appRoles, roles } from "@/utils/auth";
 
 export const LoadUsers = () => {
 	return (
-		<div>
-			{roles?.includes(appRoles.ADMINISTRATOR) ? (
-				<DashboardFrame title="List(s) of Users">
-					<UserList />
-				</DashboardFrame>
-			) : (
-				""
-			)}
-		</div>
+		<DashboardFrame title="List(s) of Users">
+			<UserList />
+		</DashboardFrame>
 	);
 };
 
