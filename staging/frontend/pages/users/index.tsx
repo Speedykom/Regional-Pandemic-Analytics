c8a62178154a7e5fcbf767344e2842f59e90ccba
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-09 07:36:25 +0000

    auth

--- staging/frontend/pages/users/index.tsx
+++ staging/frontend/pages/users/index.tsx
@@ -1,10 +1,13 @@
 import DashboardFrame from "@/components/Dashboard/DashboardFrame";
+import { Unauthorised } from "@/components/common/unauth";
 import { UserList } from "@/src/module/user/views/List";
+import { appRoles, roles } from "@/utils/auth";
 
 export const LoadUsers = () => {
+	const check: boolean = roles?.includes(appRoles.ADMINISTRATOR);
 	return (
-		<DashboardFrame title="List(s) of Users">
-			<UserList />
+		<DashboardFrame title={check ? "List(s) of Users" : "Missing Permissions"}>
+			{check ? <UserList /> : <Unauthorised />}
 		</DashboardFrame>
 	);
 };
