97726d6a3c59371bd4af07bd0a04c18c50ce2f35
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-09 04:30:48 +0000

    reafactoring ui

--- staging/frontend/src/redux/store.ts
+++ staging/frontend/src/redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { processApi } from "../src/modules/process/process";
+import { processApi } from "../modules/process/process";
+import { AuthApi } from "../modules/login/login";
 
 export const store = configureStore({
   reducer: {
     [processApi.reducerPath]: processApi.reducer,
+    [AuthApi.reducerPath]: AuthApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(processApi.middleware),
+    getDefaultMiddleware()
+      .concat(processApi.middleware)
+      .concat(AuthApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
