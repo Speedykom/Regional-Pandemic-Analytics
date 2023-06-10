9faf07c14f216cebc9b7886ef598c23562318eda
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-10 13:05:57 +0000

    refactor

--- staging/backend/hop/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HopConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hop'
--- staging/frontend/pages/process-chains/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, {  } from "react";
-import ProcessChinList from "@/src/modules/process/views/list";
-
-export default function ProcessChains() {
-  return (
-    <ProcessChinList />
-  );
-}
