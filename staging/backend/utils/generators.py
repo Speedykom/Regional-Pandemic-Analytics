a6402c27b9198a7699ea1fb2981ac5bce577fa11
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-10 13:05:57 +0000

    refactor

--- staging/backend/utils/generators.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import string
-
-# secure random string
-def get_random_secret(length: int):
-    secure_str = ''.join((secrets.choice(string.ascii_letters) for i in range(length)))
-    return secure_str
