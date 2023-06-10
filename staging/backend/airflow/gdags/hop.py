bf550b6b8bfad668ed7feeb77ba1d99d4256dba8
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-10 13:05:57 +0000

    refactor

--- staging/backend/airflow/gdags/hop.py
+++
@@ -1,8 +0,0 @@
-class EditAccessProcess:
-    def __init__(self, file):
-        self.file = file
-
-    def request_edit(self, path):
-        config = open(self.file, "w")
-        config.write(path)
-        config.close()
