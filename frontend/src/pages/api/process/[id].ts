593739c4b39359c1e4fa7695a9e5ec183ad24cb2
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-12 06:54:35 +0000

    create api for view and edit on frontend

---
+++ frontend/src/pages/api/process/[id].ts
@@ -0,0 +1,25 @@
+import { BASE_URL } from '@/common/config';
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+  const server_url = BASE_URL;
+
+  const query = req.query;
+  const { id } = query;
+
+  try {
+    const response = await axios.get(`${server_url}/api/process/one/${id}`);
+
+    if (response.status !== 200)
+      return res.status(response.status).json({ result: 'Something went wrong' });
+
+    return res.status(200).json({ ...response.data });
+    
+  } catch (error: unknown) {
+    return res.status(500).json({ result: 'Internal server error' });
+  }
+}
--- frontend/src/pages/process-chains/hop.tsx
+++ frontend/src/pages/process-chains/hop.tsx
@@ -1,7 +1,5 @@
 import DashboardFrame from "@/common/components/Dashboard/DashboardFrame";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
+import React from "react";
 import Router from "next/router";
 
 type FormValues = {
@@ -14,42 +12,6 @@ type FormValues = {
 };
 
 export default function ProcessChains() {
-  const [open, setOpen] = useState(false);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>();
-  const onSubmit = handleSubmit((data) => {
-    const dagData = {
-      dag_name: data?.dagName,
-      dag_id: data?.dagID,
-      path: data?.dagPath,
-      schedule_interval: data?.scheduleInterval,
-      parquet_path: data?.parquetPath,
-      data_source_name: data?.dataSource,
-    };
-    axios
-      .post("http://localhost:/api/airflow/", dagData, {
-        auth: {
-          username: "admin",
-          password: "back@123",
-        },
-      })
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    console.log(data);
-    setOpen(false);
-  });
-
-  const handleBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setOpen(!open);
-  };
   return (
     <DashboardFrame title="Hop Process Chains" back onBackPress={() => {
       Router.back()
