f1f60398227ef0f8dca3ac155ac09c2aaf4a08fd
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-12 08:24:06 +0000

    change the base url

--- frontend/src/common/config/index.ts
+++ frontend/src/common/config/index.ts
@@ -1,5 +1,5 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 
-export const BASE_URL = process.env.BASE_URL || "";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
 
 export const BASE_QUERY = fetchBaseQuery({ baseUrl: BASE_URL, credentials: "include" })
