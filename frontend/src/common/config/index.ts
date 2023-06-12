b5a9b5fa1800eea707f4dcaa8afdd6513ce8dba7
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-12 08:09:50 +0000

    update all the process data fetch on the app

--- frontend/src/common/config/index.ts
+++ frontend/src/common/config/index.ts
@@ -1,8 +1,5 @@
-import getConfig from "next/config";
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 
-const { publicRuntimeConfig } = getConfig();
-
-export const BASE_URL = publicRuntimeConfig.BASE_URL || "";
+export const BASE_URL = process.env.BASE_URL || "";
 
 export const BASE_QUERY = fetchBaseQuery({ baseUrl: BASE_URL, credentials: "include" })
