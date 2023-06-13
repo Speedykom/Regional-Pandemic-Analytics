0e65ace818c8606c2ebf5dcd289b08db3284a157
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-13 21:48:26 +0000

    guest token api

--- backend/superset/urls.py
+++ backend/superset/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from superset import views
 
 urlpatterns = [
-    path('superset/', views.ListDashboardsAPI.as_view()),  # dashboards
+    path('superset/', views.ListDashboardsAPI.as_view()),  # list dashboards
+    path('superset/guest/token', views.GuestTokenApi.as_view()),  # get guest token
 ]
--- backend/superset/views.py
+++ backend/superset/views.py
@@ -5,10 +5,11 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from superset.auths import get_auth_token
+import uuid
 
 class ListDashboardsAPI(APIView):
     """
-    API view to create Keycloak user
+    API view to superset dashboards
     """
     permission_classes = [AllowAny,]
     
@@ -33,4 +34,45 @@ class ListDashboardsAPI(APIView):
         
         return Response({'data': response.json()}, status=status.HTTP_200_OK)
         
+
+class GuestTokenApi(APIView):
+    """
+    API view to get superset guest token
+    """
+    permission_classes = [AllowAny,]
+    
+    #Login to superset
+    auth_response = get_auth_token()
+    
+    def get(self, request):
+        if self.auth_response['status'] != 200:
+            return Response({'errorMessage': self.auth_response['message']}, status=self.auth_response['status'])
         
+        url = f"{os.getenv('BACKEND_SUPERSET_BASE_URL')}/api/v1/security/guest_token/"
+        
+        headers = {
+            'Content-Type': "application/json",
+            'Authorization': f"Bearer ${self.auth_response['token']['access_token']}"
+        }
+        
+        payload = {
+            "user": {
+                "username": "igad_ui_",
+                "first_name": "Igad",
+                "last_name": "Frontend"
+            },
+            "resources": [{
+                "type": "dashboard",
+                "id": str(uuid.uuid4())
+            }],
+            "rls": [
+                { "clause": "publisher = 'Speedykom'" }
+            ]
+        }
+        
+        response = requests.post(url, json=payload, headers=headers)
+        
+        if response.status_code != 200:
+            return Response({'errorMessage': response.json()}, status=response.status_code)
+        
+        return Response({'data': response.json()}, status=status.HTTP_200_OK)
