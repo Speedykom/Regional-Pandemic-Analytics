8b89e089e46a580bc8669d50dbf73dd15a1c49c3
maej <contact@maej.dev>
2023-06-13 09:13:03 +0000

    Remove xml2 package and comment parseXml file content

--- frontend/src/pages/hops/parseXml.tsx
+++ frontend/src/pages/hops/parseXml.tsx
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from "react";
-import xml2js from "xml2js";
+export {};
+// import React, { useEffect, useState } from "react";
+// import xml2js from "xml2js";
 
-const ParseXml = ({ xmlData }: any) => {
-  const [data, setData] = useState();
+// const ParseXml = ({ xmlData }: any) => {
+//   const [data, setData] = useState();
 
-  useEffect(() => {
-    let parser = new xml2js.Parser();
-    parser.parseString(xmlData, function (err: any, result: any) {
-      setData(result.info);
-    });
-  }, [xmlData]);
+//   useEffect(() => {
+//     let parser = new xml2js.Parser();
+//     parser.parseString(xmlData, function (err: any, result: any) {
+//       setData(result.info);
+//     });
+//   }, [xmlData]);
 
-  return (
-    <div>
-      <table>
-        {data &&
-          Object.entries(data)?.map(([key, value]: any, index: number) => (
-            <tr key={index} className="text-gray-300">
-              <td className="text-cyan-500 font-bold">{key}:</td>
-              <td className="text-gray-400">{value}</td>
-            </tr>
-          ))}
-      </table>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <table>
+//         {data &&
+//           Object.entries(data)?.map(([key, value]: any, index: number) => (
+//             <tr key={index} className="text-gray-300">
+//               <td className="text-cyan-500 font-bold">{key}:</td>
+//               <td className="text-gray-400">{value}</td>
+//             </tr>
+//           ))}
+//       </table>
+//     </div>
+//   );
+// };
 
-export default ParseXml;
+// export default ParseXml;
