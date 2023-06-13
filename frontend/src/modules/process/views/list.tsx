84efbff3ddc3dccec751a64150099d40049647b8
maej <contact@maej.dev>
2023-06-13 08:38:08 +0000

    Add: hop modal to the process chain list pass the hop template to the process chain create method

--- frontend/src/modules/process/views/list.tsx
+++ frontend/src/modules/process/views/list.tsx
@@ -1,24 +1,30 @@
 import DashboardFrame from "@/common/components/Dashboard/DashboardFrame";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AddProcess } from "@/modules/process/views/add";
 import { Button } from "antd";
 import LoadData from "@/common/components/TABS/upload";
 import { useProcessChainList } from "../hooks";
 import { IGADTable } from "@/common/components/common/table";
 import { ViewDag } from "./view";
+import axios from "axios";
+import { getData } from "@/common/utils";
+import SelectHopModal from "@/common/components/SelectHopModal";
 
 export default function ProcessChinList() {
   const [addProcess, setProcess] = useState(false);
   const [open, setOpen] = useState(false);
   const [view, setView] = useState(false);
   const [dag, setDag] = useState<any>();
+  const [isShowHopModal, setIsShowHopModal] = useState(false);
+  const [hopData, setHopData] = useState([]);
+  const [selectedTemplate, setSelectedTemplate] = useState<object>({});
 
   const closeAdd = () => {
     setProcess(false);
   };
 
   const openAdd = () => {
-    setProcess(true);
+    setIsShowHopModal(true);
   };
 
   const closeLoad = () => {
@@ -46,6 +52,53 @@ export default function ProcessChinList() {
     viewProcess,
   });
 
+  // handle hop modal callback and hold the value returned
+  const handleHopModalResponseData = (value: any) => {
+    // if the value is an object that means it's a selected template
+    if (typeof value === "object" && value !== null) {
+      setSelectedTemplate(value);
+      setIsShowHopModal(false);
+      setProcess(true);
+    } else {
+      setIsShowHopModal(value);
+    }
+  };
+
+  const [token, setToken] = useState<string>("");
+  const fetchToken = async () => {
+    try {
+      const url = "/api/get-access-token/";
+      const response = await getData(url);
+      setToken(response?.accessToken);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const fetchHops = async () => {
+    // ${process.env.NEXT_PUBLIC_BASE_URL}
+    try {
+      const url = `http://localhost:8000/api/hop/`;
+      await axios
+        .get(url, {
+          headers: {
+            Authorization: `Token 25746e327dd2cc29ae304259574e381d9a334a39`,
+            //Bearer ${token}
+          },
+        })
+        .then((res) => {
+          setHopData(res?.data?.data);
+        });
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    // fetchToken();
+    fetchHops();
+  }, []);
+
   return (
     <>
       <DashboardFrame>
@@ -57,11 +110,7 @@ export default function ProcessChinList() {
             </p>
           </div>
           <div>
-            <Button
-              onClick={() => openAdd()}
-              type="primary"
-              size="large"
-            >
+            <Button onClick={() => openAdd()} type="primary" size="large">
               Add Process Chain
             </Button>
           </div>
@@ -74,7 +123,19 @@ export default function ProcessChinList() {
           onClose={closeView}
           key="view-dag"
         />
-        <AddProcess onClose={closeAdd} state={addProcess} />
+
+        {/* hop modal */}
+        <SelectHopModal
+          openModal={isShowHopModal}
+          parentCallback={handleHopModalResponseData}
+          hopData={hopData}
+        />
+
+        <AddProcess
+          onClose={closeAdd}
+          state={addProcess}
+          selectedTemplate={selectedTemplate}
+        />
       </DashboardFrame>
     </>
   );
