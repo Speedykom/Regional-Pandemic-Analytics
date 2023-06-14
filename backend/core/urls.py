0d428ec2c7d214ae3a5ea18614439fd2dca99736
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-14 16:19:23 +0000

    add urls to core

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', include('auth.urls')),
+    path('', include('roles.urls')),
+    path('', include('accounts.urls')),
+    path('', include('superset.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
