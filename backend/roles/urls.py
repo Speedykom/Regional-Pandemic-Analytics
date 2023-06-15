93d5afb931b75d46475ac0683dcb1b5d22add78f
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-15 12:01:01 +0000

    roles url fix

--- backend/roles/urls.py
+++ backend/roles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path('', views.CreateViewRoles.as_view()),  # create role
-    path('<str:id>', views.GetEditRole.as_view()),  # get role
-]
+# urlpatterns = [
+#     path('', views.CreateViewRoles.as_view()),  # create role
+#     path('<str:id>', views.GetEditRole.as_view()),  # get role
+# ]
