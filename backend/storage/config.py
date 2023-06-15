b5897e6c871dedc33e380340b79005396d0a3736
deathsh0ot <ala-hmz@outlook.com>
2023-06-15 09:50:37 +0100

    fixed minio issues locally

--- backend/storage/config.py
+++ backend/storage/config.py
@@ -7,8 +7,8 @@ MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
 
 MinioClient = Minio (
-    endpoint = "localhost:9000/",
-    access_key = "yMo0TxrsTQD9GhrJCwMN",
-    secret_key = "Edb6wK4WKDK4Od5veO4YeF3m9B4xnI2tfhPihvqb",
+    endpoint = MINIO_ENDPOINT,
+    access_key = MINIO_ACCESS_KEY,
+    secret_key = MINIO_SECRET_KEY,
     secure=False
 )
