fabedc5df8efd3ea148900d18da37e1dcd3dae69
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-15 06:12:15 +0000

    url patterns error fix backend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,15 +3,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from api import urls
+
 urlpatterns = [
-    path('', include('auth.urls')),
-    path('', include('roles.urls')),
-    path('', include('accounts.urls')),
-    path('', include('superset.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
