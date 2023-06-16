26417ccbc303cbbfd7761d0b88a9511ad52d563c
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-16 08:12:09 +0000

    auth superset token

--- backend/supersets/auths.py
+++ backend/supersets/auths.py
@@ -7,7 +7,7 @@ def get_auth_token ():
         'username': os.getenv('SUPERSET_USER'),
         'password': os.getenv('SUPERSET_PASS'),
         'provider':os.getenv('SUPERSET_PROVIDER'),
-        'refresh': True,
+        'refresh': False,
     }
     
     url = os.getenv('SUPERSET_LOGIN')
@@ -27,11 +27,11 @@ def get_local_auth_token ():
         'username': 'admin',
         'password': 'admin',
         'provider': 'db',
-        'refresh': True,
+        'refresh': False,
     }
     
     # url = os.getenv('SUPERSET_LOGIN')
-    url = 'http://localhost:8088/api/v1/security/token'
+    url = 'http://localhost:8088/api/v1/security/login'
     
     headers = {
         'Content-Type': 'application/json'
@@ -40,7 +40,7 @@ def get_local_auth_token ():
     response = requests.post(f"{url}", json=request_body, headers=headers)
     
     if response.status_code != 200:
-        return {'status': response.status_code, 'message': response.json()['message']}
+        return {'status': response.status_code, 'message': response.reason}
     return {'status': response.status_code, 'message': 'Access granted', 'token': response.json()}
 
 def get_csrf_token ():
@@ -48,7 +48,7 @@ def get_csrf_token ():
     # url = f"{os.getenv('SUPERSET_BASE_URL')}/security/csrf_token/"
     url = 'http://localhost:8088/api/v1/security/csrf_token/'
     
-    auth_response = get_auth_token()
+    auth_response = get_local_auth_token()
     
     if auth_response['status'] != 200:
         return {'status': auth_response['status'], 'message': auth_response['message']}
