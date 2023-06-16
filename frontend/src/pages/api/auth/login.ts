d95edd30ca6c95b4804405c599063984310b91c2
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-16 10:29:42 +0000

    embed ref setings

--- frontend/src/pages/api/auth/login.ts
+++ frontend/src/pages/api/auth/login.ts
@@ -23,6 +23,7 @@ export default async function handler(
   try {
     const response = await axios.post(`${api_url}/api/auth/key-auth`, body, {
       headers: { 'Content-Type': 'application/json' },
+      withCredentials: false
     });
 
     if (response.status !== 200)
--- frontend/src/pages/dashboards/[id].tsx
+++ frontend/src/pages/dashboards/[id].tsx
@@ -1,44 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { embedDashboard } from "@superset-ui/embedded-sdk";
 import DashboardFrame from "@/common/components/Dashboard/DashboardFrame";
 import { getGuestToken } from "@/common/utils/auth";
 
 export default function SupersetDashboard() {
-	let mountPoint: any;
-	if (typeof window !== "undefined") {
-		mountPoint = document.getElementById("igad-covid-dashboard");
-	}
-	embedDashboard({
-		id: "f3325063-a85f-46d1-88de-03f78c92d533", // given by the Superset embedding UI
-		supersetDomain: "https://localhost:8088",
-		mountPoint: mountPoint, // html element in which iframe render
-		fetchGuestToken: () =>
-			getGuestToken(`f3325063-a85f-46d1-88de-03f78c92d533`),
-		dashboardUiConfig: { hideTitle: true },
-	});
+	// let mountPoint: any;
+	const ref = useRef(null)
+	// if (typeof window !== "undefined") {
+	// 	mountPoint = document.getElementById("igad-covid-dashboard");
+	// // }
+	
+	useEffect(() => {
+		if (ref.current) {
+			embedDashboard({
+				id: "f3325063-a85f-46d1-88de-03f78c92d533", // given by the Superset embedding UI
+				supersetDomain: "http://localhost:8088",
+				mountPoint: ref.current, // html element in which iframe render
+				fetchGuestToken: () =>
+					getGuestToken(`f3325063-a85f-46d1-88de-03f78c92d533`),
+				dashboardUiConfig: { hideTitle: true },
+			});
+		}
+		
+	}, [])
 
-	// useEffect(() => {
-	//   const embed = () => {
-	//     const mountPoint = document.getElementById("igad-covid-dashboard");
-	//     if (mountPoint) {
-	//       embedDashboard({
-	//         id: `f3325063-a85f-46d1-88de-03f78c92d533`,
-	//         supersetDomain: `http://localhost:8088`,
-	//         mountPoint,
-	//         fetchGuestToken: () => getGuestToken(`f3325063-a85f-46d1-88de-03f78c92d533`),
-	//         dashboardUiConfig: {
-	//           hideTitle: true,
-	//         },
-	//       });
-	//     }
-	//   };
-	//   if (document.getElementById("igad-covid-dashboard")) {
-	//     embed();
-	//   }
-	// }, []);
 	return (
 		<DashboardFrame title={"Dashboard"}>
-			<div id="igad-covid-dashboard" />
+			<div ref={ref} id="igad-covid-dashboard" />
 		</DashboardFrame>
 	);
 }
