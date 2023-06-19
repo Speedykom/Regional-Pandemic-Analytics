d334df519cdcf6e6b02254d2d282be0bfa51e6ec
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-19 06:00:48 +0000

    fix list hop pipelines

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,8 +13,8 @@ env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-HOP_FILES_DIR = os.path.abspath(os.path.join(BASE_DIR, '../hop/pipelines/templates'))
-COPY_HOP_FILES_DIR = os.path.abspath(os.path.join(BASE_DIR, '/hop/hop_copies'))
+HOP_FILES_DIR = os.path.abspath(os.path.join(BASE_DIR, os.getenv("HOPE_TEMPLATE_PATH", "")))
+COPY_HOP_FILES_DIR = os.path.abspath(os.path.join(BASE_DIR, os.getenv("HOPE_PIPELINE_PATH", "")))
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
