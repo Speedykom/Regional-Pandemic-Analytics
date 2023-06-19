13927cd4b66826d71482f200db731472ae93de6c
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-19 09:10:41 +0000

    superset proxy config

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -270,7 +270,7 @@ SHOW_STACKTRACE = True
 
 # Use all X-Forwarded headers when ENABLE_PROXY_FIX is True.
 # When proxying to a different port, set "x_port" to 0 to avoid downstream issues.
-ENABLE_PROXY_FIX = True
+ENABLE_PROXY_FIX = False
 # PROXY_FIX_CONFIG = {"x_for": 1, "x_proto": 1, "x_host": 1, "x_port": 0, "x_prefix": 1}
 
 # Configuration for scheduling queries from SQL Lab.
