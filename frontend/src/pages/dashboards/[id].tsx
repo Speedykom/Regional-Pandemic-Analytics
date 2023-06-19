423b9a90dada251450ae95a094247daa97f122f3
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-19 08:13:39 +0000

    superset nginx error

--- frontend/src/pages/dashboards/[id].tsx
+++ frontend/src/pages/dashboards/[id].tsx
@@ -1,19 +1,50 @@
 import { useEffect, useRef, useState } from "react";
 import { embedDashboard } from "@superset-ui/embedded-sdk";
 import DashboardFrame from "@/common/components/Dashboard/DashboardFrame";
-import { getGuestToken } from "@/common/utils/auth";
+import { api_url, getGuestToken } from "@/common/utils/auth";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function SupersetDashboard() {
 	let ref = useRef<HTMLDivElement>(null);
+	const [uuid, setUuid] = useState('')
+	const router = useRouter()
+
+	const viewDash = async(id: string) => {
+		const response = await axios.get(
+			`${api_url}/api/superset/dashboard/embed/${router.query?.id}`, {
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		)
+		if (response.status === 404) {
+			await axios.post(
+				`${api_url}/api/superset/dashboard/enable-embed`, {
+					uid: router.query?.id
+				}, {
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				}
+			).then((res) => {
+				const dashboardUUID = res?.data?.result?.uuid;
+				setUuid(dashboardUUID)
+				
+			})
+		} else {
+			setUuid(response.data?.result?.uuid)
+		}
+	};
 
 	const embedDash = async () => {
 		if (ref.current) {
 			await embedDashboard({
-				id: "f02c414f-93c1-4dad-9b21-fe383700b02c", // given by the Superset embedding UI
+				id: uuid, // given by the Superset embedding UI
 				supersetDomain: "http://superset:8088/",
 				mountPoint: ref.current, // html element in which iframe render
 				fetchGuestToken: () =>
-					getGuestToken(`f02c414f-93c1-4dad-9b21-fe383700b02c`),
+					getGuestToken(uuid),
 				dashboardUiConfig: {
 					hideTitle: true,
 					hideTab: true,
