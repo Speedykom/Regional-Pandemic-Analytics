076b12c34c2c975ae3679e10eec4058eafd01e9b
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-19 06:00:48 +0000

    fix list hop pipelines

--- frontend/src/common/redux/store.ts
+++ frontend/src/common/redux/store.ts
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { processApi } from "../../modules/process/process";
 import { AuthApi } from "../../modules/login/login";
+import { templateApi } from "@/modules/template/template";
 
 export const store = configureStore({
   reducer: {
     [processApi.reducerPath]: processApi.reducer,
+    [templateApi.reducerPath]: templateApi.reducer,
     [AuthApi.reducerPath]: AuthApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(processApi.middleware)
-      .concat(AuthApi.middleware),
+      .concat(AuthApi.middleware)
+      .concat(templateApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
