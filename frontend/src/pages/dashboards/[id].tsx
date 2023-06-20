ae27a7b3f2d86ceb8fd4540a34fb47282820c7e3
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-20 08:46:30 +0000

    frontend superset

--- frontend/src/pages/dashboards/[id].tsx
+++ frontend/src/pages/dashboards/[id].tsx
@@ -10,41 +10,41 @@ export default function SupersetDashboard() {
 	const [uuid, setUuid] = useState('')
 	const router = useRouter()
 
-	const viewDash = async(id: string) => {
-		const response = await axios.get(
-			`${api_url}/api/superset/dashboard/embed/${router.query?.id}`, {
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}
-		)
-		if (response.status === 404) {
-			await axios.post(
-				`${api_url}/api/superset/dashboard/enable-embed`, {
-					uid: router.query?.id
-				}, {
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				}
-			).then((res) => {
-				const dashboardUUID = res?.data?.result?.uuid;
-				setUuid(dashboardUUID)
+	// const viewDash = async(id: string) => {
+	// 	const response = await axios.get(
+	// 		`${api_url}/api/superset/dashboard/embed/${router.query?.id}`, {
+	// 			headers: {
+	// 				'Content-Type': 'application/json'
+	// 			}
+	// 		}
+	// 	)
+	// 	if (response.status === 404) {
+	// 		await axios.post(
+	// 			`${api_url}/api/superset/dashboard/enable-embed`, {
+	// 				uid: router.query?.id
+	// 			}, {
+	// 				headers: {
+	// 					'Content-Type': 'application/json'
+	// 				}
+	// 			}
+	// 		).then((res) => {
+	// 			const dashboardUUID = res?.data?.result?.uuid;
+	// 			setUuid(dashboardUUID)
 				
-			})
-		} else {
-			setUuid(response.data?.result?.uuid)
-		}
-	};
+	// 		})
+	// 	} else {
+	// 		setUuid(response.data?.result?.uuid)
+	// 	}
+	// };
 
 	const embedDash = async () => {
 		if (ref.current) {
 			await embedDashboard({
-				id: uuid, // given by the Superset embedding UI
-				supersetDomain: `${process.env.NEXT_PUBLIC_SUPERSET_URL}`,
+				id: '381943fb-48c1-42ba-a85f-78b49bd14519', // given by the Superset embedding UI
+				supersetDomain: `http://localhost:8088/`,
 				mountPoint: ref.current, // html element in which iframe render
 				fetchGuestToken: () =>
-					getGuestToken(uuid),
+					getGuestToken('381943fb-48c1-42ba-a85f-78b49bd14519'),
 				dashboardUiConfig: {
 					hideTitle: true,
 					hideTab: true,
