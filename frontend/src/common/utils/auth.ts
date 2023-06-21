a3184147d6b89be748023219c24d9e7e4686af8d
Mohamed James Kamara <james.kamara@speedykom.de>
2023-06-21 18:12:36 +0000

    logout from frontend and keycloak api implemented in frontend

--- frontend/src/common/utils/auth.ts
+++ frontend/src/common/utils/auth.ts
@@ -1,21 +1,9 @@
 import axios from "axios";
-import secureLocalStorage from "react-secure-storage";
-
-export const api_url = process.env.NEXT_PUBLIC_BASE_URL;
-
-const user: any = secureLocalStorage.getItem("user") as object;
-
-export const role: string = user?.realm_access?.roles;
-
-export enum appRoles {
-    ADMINISTRATOR = 'Administrator',
-    DHO = 'District Health Officer (DHO)',
-    M_AND_E = 'Monitoring and Evaluation (M&E) Expert'
-}
+import { BASE_URL } from "../config";
 
 export const getUserRole = async (roles: []) => {
     const rolee = roles.find(item => item != 'default-roles-stack')
-    return await axios.get(`${api_url}/api/role/${rolee}?type=name`).then((res) => {
+    return await axios.get(`${BASE_URL}/api/role/${rolee}?type=name`).then((res) => {
         let newRole = res?.data?.role;
         let roleAttributeKeys = Object.keys(newRole?.attributes);
         roleAttributeKeys.forEach((key, _) => {
@@ -31,7 +19,7 @@ export const getUserRole = async (roles: []) => {
 export const getGuestToken = async (uuid: string): Promise<string> => {
     return new Promise<string>((resolve) => {
         console.log("====> Calling token!")
-        axios.post(`${api_url}/api/superset/guest/token`, { id: uuid }).then((response) => {
+        axios.post(`${BASE_URL}/api/superset/guest/token`, { id: uuid }).then((response) => {
             console.log(response.status);
             resolve(response.data?.token);
         }).catch((err) => {
@@ -40,3 +28,4 @@ export const getGuestToken = async (uuid: string): Promise<string> => {
     })
     
 }
+
