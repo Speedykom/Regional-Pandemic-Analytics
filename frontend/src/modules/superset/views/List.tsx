30a4c4958151ef836131f7651660eb215031be27
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-21 00:23:17 +0000

    fix conflict

--- frontend/src/modules/superset/views/List.tsx
+++ frontend/src/modules/superset/views/List.tsx
@@ -1,47 +1,21 @@
 import { useDashboards } from "../hooks";
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { useRouter } from "next/router";
-import { api_url } from "@/common/utils/auth";
 import { IGADTable } from "@/common/components/common/table";
 import { IUser } from "@/modules/user/interface";
+import { BASE_URL } from "@/common/config";
 
 export const DashboardList = () => {
 	const [data, setData] = useState<Array<IUser>>([]);
 	const [loading, setLoading] = useState<boolean>(true);
 	const router = useRouter();
 
-	const token = async () => {
-		return await axios
-			.post(
-				`http://localhost:8088/api/v1/security/login`,
-				{
-					username: "admin",
-					password: "admin",
-					provider: "db",
-					refresh: true,
-				},
-				{
-					headers: {
-						Accept: "application/json",
-					},
-				}
-			)
-			.then((res) => {
-				return res?.data?.access_token;
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
-
 	const fetchDashboards = async () => {
 		// const myToken = await token();
 		setLoading(true);
-		const url = `http://localhost:3000/api/superset/get-list`;
+		const url = `${BASE_URL}/api/superset/list`;
 		const data = await fetch(url)
 		if (data.status == 200) {
-			console.log(data)
 			data.json().then((d) => {
 				setLoading(false)
 				setData(d?.result)
@@ -49,18 +23,6 @@ export const DashboardList = () => {
 		} else {
 			console.log({error: data})
 		}
-		// await axios
-		// 	.get(url, {
-		// 		headers: {
-		// 			"Content-Type": "application/json",
-		// 			Authorization: `Bearer ${myToken}`,
-		// 		},
-		// 	})
-		// 	.then((res) => {
-		// 	console.log({res})
-		// 		setLoading(false);
-		// 		setData(res?.data?.result);
-		// 	});
 	};
 
 	const rowAction = (id: string) => {
