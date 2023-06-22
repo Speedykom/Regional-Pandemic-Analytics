418064e6929ca6706abcdfb4feda76237ed7673c
Foday S.N Kamara <fodaysnkamara@gmail.com>
2023-06-22 14:30:02 +0000

    Fix: responsiveness of Remember Me and password text for mobile

--- frontend/src/modules/login/views/login.tsx
+++ frontend/src/modules/login/views/login.tsx
@@ -12,7 +12,7 @@ import { getUserRole } from "@/common/utils/auth";
 export default function LoginForm() {
   const [loading, setLoading] = useState(false);
   const [form] = Form.useForm();
-  const router = useRouter();  
+  const router = useRouter();
 
   const onFinish = (data: any) => {
     setLoading(true);
@@ -26,7 +26,7 @@ export default function LoginForm() {
       .then(async (res: any) => {
         if (res.status == 200) {
           let payload: any = jwt_decode(res?.data?.result?.access_token);
-          const role = await getUserRole(payload?.realm_access?.roles)
+          const role = await getUserRole(payload?.realm_access?.roles);
           secureLocalStorage.setItem("user_role", role);
 
           // @ts-ignore
@@ -55,107 +55,111 @@ export default function LoginForm() {
         <title>Regional Pandemic Analytics Tool | Welcome</title>
       </Head>
       <section className="gradient-form md:h-screen">
-          <div className="container mx-auto px-6" style={{marginTop:"10vh"}}>
-            <div className="flex justify-center items-center flex-wrap g-6 text-gray-800">
-              <div className="xl:w-10/12">
-                <div className="block bg-white shadow-lg rounded-lg">
-                  <div className="lg:flex lg:flex-wrap g-0">
-                    <div className="lg:w-6/12 px-4 md:px-0">
-                      <div className="md:p-12 md:mx-6">
-                        <div className="text-center">
-                          <Image
-                            className="mx-auto w-72"
-                            src="/images/igad_logo.jpg"
-                            alt="logo"
-                            width={500}
-                            height={200}
-                          />
+        <div className="container mx-auto px-6" style={{ marginTop: "10vh" }}>
+          <div className="flex justify-center items-center flex-wrap g-6 text-gray-800">
+            <div className="xl:w-10/12">
+              <div className="block bg-white shadow-lg rounded-lg">
+                <div className="lg:flex lg:flex-wrap g-0">
+                  <div className="lg:w-6/12 px-4 md:px-0">
+                    <div className="md:p-12 md:mx-6">
+                      <div className="text-center">
+                        <Image
+                          className="mx-auto w-72"
+                          src="/images/igad_logo.jpg"
+                          alt="logo"
+                          width={500}
+                          height={200}
+                        />
+                      </div>
+                      <Form
+                        form={form}
+                        name="login-user"
+                        onFinish={onFinish}
+                        layout="vertical"
+                        scrollToFirstError
+                        className="px-8"
+                      >
+                        <div>
+                          <Form.Item
+                            name="username"
+                            rules={[
+                              {
+                                required: true,
+                                message: "Please input your username",
+                              },
+                            ]}
+                          >
+                            <Input
+                              size="large"
+                              placeholder="Username"
+                              className="w-full"
+                            />
+                          </Form.Item>
                         </div>
-                        <Form
-              form={form}
-              name="login-user"
-              onFinish={onFinish}
-              layout="vertical"
-              scrollToFirstError
-              className="px-8"
-            >
-              <div>
-                <Form.Item
-                  name="username"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your username",
-                    },
-                  ]}
-                >
-                  <Input
-                    size="large"
-                    placeholder="Username"
-                    className="w-full"
-                  />
-                </Form.Item>
-              </div>
-              <Form.Item
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your password",
-                  },
-                ]}
-              >
-                <Input
-                  size="large"
-                  type="password"
-                  placeholder="Password"
-                  className="w-full"
-                />
-              </Form.Item>
-              <div className="w-full flex justify-between mb-3 pt-2">
-                <div className="w-1/2">
-                  <Checkbox>Remember Me</Checkbox>
-                </div>
-                <div>
-                  <Button
-                    type="text"
-                    onClick={() => router.push("/users/reset-password")}
-                  >
-                    Forgot password
-                  </Button>
-                </div>
-              </div>
-              <div className="mb-5">
-                <Button
-                  loading={loading}
-                  type="primary"
-                  onClick={() => form.submit()}
-                  size="large"
-                  className="w-full"
-                >
-                  Login
-                </Button>
-              </div>
-            </Form>
-                      </div>
+                        <Form.Item
+                          name="password"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Please input your password",
+                            },
+                          ]}
+                        >
+                          <Input
+                            size="large"
+                            type="password"
+                            placeholder="Password"
+                            className="w-full"
+                          />
+                        </Form.Item>
+                        <div className="w-full flex flex-col mb-3 pt-2 md:flex-row md:justify-between">
+                          <div className="w-full md:w-1/2 md:mb-0">
+                            <Checkbox>Remember Me</Checkbox>
+                          </div>
+                          <div className="w-full md:w-auto">
+                            <a
+                              onClick={() =>
+                                router.push("/users/reset-password")
+                              }
+                            >
+                              Forgot password
+                            </a>
+                          </div>
+                        </div>
+
+                        <div className="mb-5">
+                          <Button
+                            loading={loading}
+                            type="primary"
+                            onClick={() => form.submit()}
+                            size="large"
+                            className="w-full"
+                          >
+                            Login
+                          </Button>
+                        </div>
+                      </Form>
                     </div>
-                    <div className="bg-emerald-700 lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none">
-                      <div className="text-white px-4 py-6 md:p-12 md:mx-6">
-                        <h4 className="text-2xl font-semibold mb-4">
-                          Welcome back!
-                        </h4>
-                        <p className="text-sm">
-                          Simply login to access the IGAD regional pandemic analytics tool to collect, analyze,
-                          and report granular and aggregated data from multiple sources for informed decision-making.
-                        </p>
-                      </div>
+                  </div>
+                  <div className="bg-emerald-700 lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none">
+                    <div className="text-white px-4 py-6 md:p-12 md:mx-6">
+                      <h4 className="text-2xl font-semibold mb-4">
+                        Welcome back!
+                      </h4>
+                      <p className="text-sm">
+                        Simply login to access the IGAD regional pandemic
+                        analytics tool to collect, analyze, and report granular
+                        and aggregated data from multiple sources for informed
+                        decision-making.
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </>
   );
 }
