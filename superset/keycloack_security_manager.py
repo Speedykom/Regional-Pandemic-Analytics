e9a4e2bf0e45617838e233df10b2c2e965ba2abc
deathsh0ot <ala-hmz@outlook.com>
2023-06-25 16:03:29 +0100

    added superset roles implementation

--- superset/keycloack_security_manager.py
+++ superset/keycloack_security_manager.py
@@ -5,8 +5,20 @@ from flask_oidc import OpenIDConnect
 from flask_appbuilder.security.views import AuthOIDView
 from flask_login import login_user
 from urllib.parse import quote
-from flask_appbuilder.views import expose
+from flask_appbuilder.views import ModelView, SimpleFormView, expose
+import logging
 import urllib.parse
+import os
+import jwt
+import requests
+from base64 import b64decode
+from cryptography.hazmat.primitives import serialization
+
+CLIENT_ID = os.getenv("SUPERSET_CLIENT_ID",'superset')
+CLIENT_SECRET = os.getenv("SUPERSET_CLIENT_SECRET")
+KEYCLOAK_KC_HOSTNAME_URL=os.getenv("KEYCLOAK_KC_HOSTNAME_URL")
+OIDC_ISSUER = os.getenv("OIDC_ISSUER")
+APP_REALM = os.getenv("APP_REALM", "regional-pandemic-analytics")
 
 class OIDCSecurityManager(SupersetSecurityManager):
 
@@ -16,6 +28,28 @@ class OIDCSecurityManager(SupersetSecurityManager):
             self.oid = OpenIDConnect(self.appbuilder.get_app)
         self.authoidview = AuthOIDCView
 
+req = requests.get(OIDC_ISSUER)
+key_der_base64 = req.json()["public_key"]
+key_der = b64decode(key_der_base64.encode())
+public_key = serialization.load_der_public_key(key_der)
+
+def get_roles(input_array):
+    AUTH_ROLES_MAPPING = {
+        "superset_admin": ["Admin"],
+        "superset_public": ["Public"],
+        "superset_alpha": ["Alpha"],
+        "superset_gamma": ["Gamma"],
+        "superset_granter": ["granter"],
+        "superset_sql_lab": ["sql_lab"],
+    }
+    
+    roles = []
+    for item in input_array:
+        if item in AUTH_ROLES_MAPPING:
+            roles.extend(AUTH_ROLES_MAPPING[item])
+    
+    return roles
+
 class AuthOIDCView(AuthOIDView):
 
     @expose('/login/', methods=['GET', 'POST'])
@@ -24,25 +58,34 @@ class AuthOIDCView(AuthOIDView):
         oidc = sm.oid
 
         @self.appbuilder.sm.oid.require_login
+       
         def handle_login():
             user = sm.auth_user_oid(oidc.user_getfield('email'))
-
             if user is None:
-                info = oidc.user_getinfo(['preferred_username', 'given_name', 'family_name', 'email','resource_access'])
+                info = oidc.user_getinfo(['preferred_username', 'given_name', 'family_name', 'email' ])
                 firstname = ""
                 lastname = ""
+                roles=["Gamma"]
+                
+                if(oidc.get_access_token()):
+                    token = oidc.get_access_token()
+                    decodedToken = jwt.decode(token, public_key, algorithms=['RS256'], audience="realm-management", issuer=KEYCLOAK_KC_HOSTNAME_URL+"/realms/"+APP_REALM)
+                    superset_roles = decodedToken["resource_access"]["superset"]["roles"]
+                    roles=get_roles(superset_roles)
 
-                if not info.get('given_name'):
-                    firstname = info.get('preferred_username')
+                    if not info.get('given_name'):
+                        firstname = info.get('preferred_username')
+                    else:
+                        firstname = info.get('given_name')
+                    if not info.get('family_name'):
+                        lastname = info.get('preferred_username')
+                    else:
+                        lastname = info.get('family_name')
+                    user = sm.add_user(info.get('preferred_username'), firstname, lastname,
+                                    info.get('email'), [sm.find_role(role) for role in roles])
                 else:
-                    firstname = info.get('given_name')
-                if not info.get('family_name'):
-                    lastname = info.get('preferred_username')
-                else:
-                    lastname = info.get('family_name')
-                user = sm.add_user(info.get('preferred_username'), firstname, lastname,
-                                   info.get('email'), sm.find_role('Admin'))
-
+                    return self.backchannel_logout()
+                
             login_user(user, remember=False)
             return redirect(self.appbuilder.get_url_for_index)
 
@@ -58,7 +101,6 @@ class AuthOIDCView(AuthOIDView):
 
         return redirect(
             oidc.client_secrets.get('issuer') + '/protocol/openid-connect/logout?client_id=' + oidc.client_secrets.get('client_id') + '&logout_redirect_uri=' + quote(redirect_url))
-
 
     @expose('/backchannel-logout/', methods=['GET', 'POST'])
     def backchannel_logout(self):
