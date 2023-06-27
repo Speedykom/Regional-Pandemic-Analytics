f486c6d3867b68c9d51c8bdfcba13afb48589565
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-06-27 16:13:20 +0000

    refactor

--- frontend/src/modules/process/views/hop.tsx
+++ frontend/src/modules/process/views/hop.tsx
@@ -1,39 +1,6 @@
-import { theme } from "antd";
-import { useState } from "react";
-
 export const EditHopProcess = () => {
-  const { token } = theme.useToken();
-  const [current, setCurrent] = useState(0);
-
-  const next = () => {
-    setCurrent(current + 1);
-  };
-
-  const prev = () => {
-    setCurrent(current - 1);
-  };
-
-  const steps = [
-    {
-      title: "Orchestration",
-      content: "First-content",
-    },
-    {
-      title: "Integration",
-      content: "Second-content",
-    },
-    {
-      title: "Data Source",
-      content: "Last-content",
-    },
-    {
-      title: "Analytics",
-      content: "Last-content",
-    },
-  ];
-
-  const items = steps.map((item) => ({ ...item, key: item.title }));
-
+  console.log(process.env.NEXT_PUBLIC_HOP_UI);
+  
   return (
     <div>
       <iframe style={{height: "700px", width: "100%"}} src={process.env.NEXT_PUBLIC_HOP_UI} />
