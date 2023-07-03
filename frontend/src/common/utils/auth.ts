2495ccfdad355d62d38f798087f2b17befeda464
deathsh0ot <ala-hmz@outlook.com>
2023-07-03 17:59:22 +0100

    feat: now dashboard embedding is functional

--- frontend/src/common/utils/auth.ts
+++ frontend/src/common/utils/auth.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../config";
+import secureLocalStorage from "react-secure-storage";
 
 export const getUserRole = async (roles: []) => {
     const rolee = roles.find(item => item != 'default-roles-stack')
@@ -15,10 +16,17 @@ export const getUserRole = async (roles: []) => {
         console.log(err)
     })
 }
-
+const tokens: any = secureLocalStorage.getItem("tokens");
+const accessToken = tokens && 'accessToken' in tokens ? tokens.accessToken : '' 
 export const getGuestToken = async (uuid: string): Promise<string> => {
     return new Promise<string>((resolve) => {
-        axios.post(`${BASE_URL}/api/superset/guest/token`, { id: uuid }).then((response) => {
+        axios.post(`${BASE_URL}/api/superset/guest/token`, { id: uuid },{
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${accessToken}`
+            },
+        }).then((response) => {
             console.log(response.status);
             resolve(response.data?.token);
         }).catch((err) => {
