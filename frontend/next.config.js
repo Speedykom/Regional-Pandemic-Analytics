3666f9c5a309c5fd0afdedf9d7fe60583610c83b
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-07-06 17:28:01 +0000

    Merge branch 'main' into feature/procss-chain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   serverRuntimeConfig: {
     // Will only be available on the server side
-    NEXT_PRIVATE_BASE_URL: "http://localhost:8000", // Pass through env variables
+    NEXT_PRIVATE_BASE_URL: "http://127.0.0.1:8000", // Pass through env variables
   },
   publicRuntimeConfig: {
     NEXT_PUBLIC_MINIO_URL: "http://localhost:9001",
-    NEXT_PUBLIC_BASE_URL: "http://localhost:8000",
+    NEXT_PUBLIC_BASE_URL: "http://127.0.0.1:8000",
     NEXT_PUBLIC_SUPERSET_URL: "http://localhost:8883",
     NEXT_HOP_UI: "http://localhost:8882/ui",
   },
