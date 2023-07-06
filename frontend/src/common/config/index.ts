eb82a5dbfc639c8c23d0f0607104a1d32d8e9692
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-07-06 07:28:30 +0000

    fix process ui

--- frontend/src/common/config/index.ts
+++ frontend/src/common/config/index.ts
@@ -1,2 +1,16 @@
+import secureLocalStorage from "react-secure-storage";
+
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const SUPERSET_URL = process.env.NEXT_PUBLIC_SUPERSET_URL
+
+export const prepareHeaders = (headers: any, { getState, endpoint }: any) => {
+    const tokens = secureLocalStorage.getItem("tokens");
+
+    if (!tokens) headers;
+
+    const { accessToken } = (tokens as any);
+
+    headers.set("AUTHORIZATION", `Bearer ${accessToken}`);
+
+    return headers;
+  }
