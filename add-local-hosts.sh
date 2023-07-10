b075d7e4b3475235d91f6a0b0a1a166eaa354df6
sihamouda <anis.c.hamouda@gmail.com>
2023-07-10 17:24:41 +0100

    feat: openhim + keycloak + https

--- add-local-hosts.sh
+++ add-local-hosts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-hosts="127.0.0.1 frontend.igad.local backend.igad.local keycloak.igad.local airflow.igad.local console.minio.igad.local minio.igad.local superset.igad.local hop.igad.local druid.igad.local openhim.igad.local"
+hosts="127.0.0.1 frontend.igad.local backend.igad.local keycloak.igad.local airflow.igad.local console.minio.igad.local minio.igad.local superset.igad.local hop.igad.local druid.igad.local openhim.igad.local console.openhim.igad.local"
 if grep -q "$hosts" /etc/hosts; then
     echo hosts already added!
 else    
