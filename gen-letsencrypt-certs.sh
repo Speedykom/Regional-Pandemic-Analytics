6d66b65e5b2e0a1de1ee36d44c2e322ca1943908
zbidi <hamza@speedykom.de>
2023-07-11 12:39:27 +0100

    fix: gen letsencrypt certs

--- gen-letsencrypt-certs.sh
+++ gen-letsencrypt-certs.sh
@@ -5,56 +5,61 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(analytics2.igad-health.eu coordinator2.igad-health.eu db2.igad-health.eu integration2.igad-health.eu auth2.igad-health.eu data2.igad-health.eu orchestration2.igad-health.eu cache2.igad-health.eu data2.igad-health.eu home2.igad-health.eu)
 rsa_key_size=4096
 data_path="./certbot"
 email="hamza@speedykom.de" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
+domain_names="analytics2.igad-health.eu coordinator2.igad-health.eu db2.igad-health.eu integration2.igad-health.eu auth2.igad-health.eu data2.igad-health.eu orchestration2.igad-health.eu cache2.igad-health.eu data2.igad-health.eu home2.igad-health.eu"
+#Geneate certificates for each service
+for domains in $domain_names; do
+
+  if [ -d "$data_path" ]; then
+    read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+    if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+      exit
+    fi
   fi
-fi
 
 
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+    echo "### Downloading recommended TLS parameters ..."
+    mkdir -p "$data_path/conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+    echo
+  fi
+
+  echo "### Creating dummy certificate for $domains ..."
+  path="/etc/letsencrypt/live/$domains"
+  mkdir -p "$data_path/conf/live/$domains"
+  docker compose run --rm --entrypoint "\
+    openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+      -keyout '$path/privkey.pem' \
+      -out '$path/fullchain.pem' \
+      -subj '/CN=localhost'" certbot
   echo
-fi
-
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
 
 
-echo "### Starting nginx ..."
-docker compose --env-file ./.env -f docker-compose.yml -f docker-compose.dev.yml  up --force-recreate -d nginx
-echo
+  echo "### Starting nginx ..."
+  docker compose --env-file ./.env -f docker-compose.yml -f docker-compose.dev.yml  up --force-recreate -d nginx
+  echo
 
-echo "### Deleting dummy certificate for $domains ..."
-docker compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
+  echo "### Deleting dummy certificate for $domains ..."
+  docker compose run --rm --entrypoint "\
+    rm -Rf /etc/letsencrypt/live/$domains && \
+    rm -Rf /etc/letsencrypt/archive/$domains && \
+    rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
+  echo "### Requesting Let's Encrypt certificate for $domains ..."
+  #Join $domains to -d args
+  domain_args=""
+  for domain in "${domains[@]}"; do
+    domain_args="$domain_args -d $domain"
+  done
+
 done
 
 # Select appropriate email arg
