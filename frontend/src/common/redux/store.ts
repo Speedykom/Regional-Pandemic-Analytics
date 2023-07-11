d7ade32127d26bfe368ad40e66dad3cbb8b786d1
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-07-11 09:56:40 +0000

    Remove refresh token from auth an move to logout

--- frontend/src/common/redux/store.ts
+++ frontend/src/common/redux/store.ts
@@ -2,17 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { processApi } from "../../modules/process/process";
 import { AuthApi } from "../../modules/auth/auth";
 import { pipelineApi } from "@/modules/pipeline/pipeline";
+import { LogoutApi } from "@/modules/auth/logout";
 
 export const store = configureStore({
   reducer: {
     [processApi.reducerPath]: processApi.reducer,
     [pipelineApi.reducerPath]: pipelineApi.reducer,
     [AuthApi.reducerPath]: AuthApi.reducer,
+    [LogoutApi.reducerPath]: LogoutApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(processApi.middleware)
       .concat(AuthApi.middleware)
+      .concat(LogoutApi.middleware)
       .concat(pipelineApi.middleware),
 });
 
