9eb79e4af2beff8ba7b5571c02727fd8098aeac9
zbidi <hamza@speedykom.de>
2023-07-11 12:31:13 +0100

    fix: refactor nginx + certs

--- gen-local-certs.sh
+++ gen-local-certs.sh
@@ -8,10 +8,10 @@ organization=Speedykom
 organizationalunit=IT
 
 #Where the certs would be generated
-cd nginx/certificates
-
+cd ./certbot/conf/live
 #Generate CA and CAkey if they don't already exist
 CA=myCA.pem
+commonname=myCA
 if test -f "$CA"; then
     echo "$FILE already exists."
 else 
@@ -19,7 +19,6 @@ else
     openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem \
          -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/"
     echo files generated!
-
 fi
 
 #Our services
@@ -27,11 +26,12 @@ service_table="frontend backend airflow druid hop keycloak minio console-minio s
 #Geneate certificates for each service
 for service in $service_table; do
 commonname=$service
-openssl genrsa -out $service.key 2048
-openssl req -new -key $service.key -out $service.csr \
+mkdir $service.igad.local
+openssl genrsa -out $service.igad.local/privkey.pem 2048
+openssl req -new -key $service.igad.local/privkey.pem -out $service.igad.local/$service.csr \
     -subj "/C=$country/1=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/"
 
-cat > $service.ext << EOF
+cat > $service.igad.local/$service.ext << EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -40,6 +40,6 @@ subjectAltName = @alt_names
 DNS.1 = $service
 EOF
 
-openssl x509 -req -in $service.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial \
--out $service.crt -days 825 -sha256 -extfile $service.ext
+openssl x509 -req -in $service.igad.local/$service.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial \
+-out $service.igad.local/fullchain.pem -days 825 -sha256 -extfile $service.igad.local/$service.ext
 done
