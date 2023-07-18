660678c67df8e71568bc3a9130b2e50e0a129f40
Mohamed James Kamara <james.kamara@speedykom.de>
2023-07-18 02:35:19 +0000

    comment scopes in list users api

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -216,12 +216,12 @@ class ListUsersAPI(APIView):
     """
     API view to get all users
     """
-    keycloak_scopes = {	
-        'GET': 'user:read',	
-        'POST': 'user:create',	
-        'PUT': 'user:update',	
-        'DELETE': 'user:delete'	
-    } 
+    # keycloak_scopes = {	
+    #     'GET': 'user:read',	
+    #     'POST': 'user:create',	
+    #     'PUT': 'user:update',	
+    #     'DELETE': 'user:delete'	
+    # } 
      
     permission_classes = [AllowAny, ]
 
