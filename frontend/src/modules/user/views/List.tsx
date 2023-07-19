395905aa1ad155ccab964fa2590c594c080db796
Mohamed James Kamara <james.kamara@speedykom.de>
2023-07-19 09:29:28 +0000

    user list view responsive

--- frontend/src/modules/user/views/List.tsx
+++ frontend/src/modules/user/views/List.tsx
@@ -3,8 +3,19 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import getConfig from "next/config";
 import secureLocalStorage from "react-secure-storage";
-import { IRole } from "@/modules/roles/interface";
-import { Card, Text, Badge } from "@tremor/react";
+import {
+	Card,
+	Text,
+	Badge,
+	Table,
+	Button,
+	Flex,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeaderCell,
+	TableRow,
+} from "@tremor/react";
 import {
 	CheckIcon,
 	ExclamationCircleIcon,
@@ -14,18 +25,16 @@ import {
 	WifiIcon,
 	XMarkIcon,
 } from "@heroicons/react/24/outline";
-import { FiMoreVertical } from "react-icons/fi";
-import { useRouter } from "next/router";
+import router, { useRouter } from "next/router";
 import { Loading } from "@/common/components/Loading";
 const { publicRuntimeConfig } = getConfig();
+import MediaQuery from "react-responsive";
+import { UserListMobile } from "./UserMobile";
 
 export const UserList = () => {
 	const [open, setOpen] = useState<boolean>(false);
 	const [data, setData] = useState<Array<IUser>>([]);
 	const [loading, setLoading] = useState<boolean>(true);
-	const [roles, setRoles] = useState([]);
-	const [userRoles, setUserRoles] = useState<Array<IRole>>([]);
-	const [roleLoading, setRoleLoading] = useState(true);
 	const router = useRouter();
 
 	const tokens: any = secureLocalStorage.getItem("tokens") as object;
@@ -56,28 +65,7 @@ export const UserList = () => {
 		fetchUsers();
 	};
 
-	const fetchRoles = async () => {
-		try {
-			setRoleLoading(true);
-			const url = `${publicRuntimeConfig.NEXT_PUBLIC_BASE_URL}/api/role`;
-			await axios
-				.get(url, {
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${accessToken}`,
-					},
-				})
-				.then((res) => {
-					setRoleLoading(false);
-					setRoles(res?.data);
-				});
-		} catch (error) {
-			console.error("Error:", error);
-		}
-	};
-
 	useEffect(() => {
-		fetchRoles();
 		fetchUsers();
 	}, []);
 
@@ -91,91 +79,146 @@ export const UserList = () => {
 					</p>
 				</div>
 			</nav>
-			<div className="container mb-5">
-				{loading ? (
-					<Loading />
-				) : (
-					<div className="flex flex-wrap">
-						{data.map((item, index) => (
-							<div className="lg:w-1/3 lg:p-2 md:py-2 md:w-full w-full">
-								<Card>
-									<div className="flex space-x-4">
-										<div className="h-12 w-12 mr-2 overflow-hidden rounded-full flex items-center justify-center border border-gray-300">
-											<img
-												src={
-													item?.attributes?.avatar &&
-													item?.attributes?.avatar[0] != ""
-														? item?.attributes?.avatar[0]
-														: "/avater.png"
-												}
-												className="w-full h-full"
-											/>
-										</div>
-										<div className="flex-1 space-y-4 py-1">
-											<div className="h-4 rounded w-3/4 flex space-x-2">
-												<Text className="text-lg font-sans">
+			<MediaQuery maxWidth={1224}>
+				<UserListMobile loading={loading} data={data} />
+			</MediaQuery>
+			<MediaQuery minWidth={1824}>
+				<div>
+					<Card>
+						<Table className="mt-5">
+							<TableHead>
+								<TableRow>
+									<TableHeaderCell>Full Name</TableHeaderCell>
+									<TableHeaderCell>Username</TableHeaderCell>
+									<TableHeaderCell>Email</TableHeaderCell>
+									<TableHeaderCell>Phone</TableHeaderCell>
+									<TableHeaderCell>Gender</TableHeaderCell>
+									<TableHeaderCell>Country</TableHeaderCell>
+									<TableHeaderCell>Email Verified</TableHeaderCell>
+									<TableHeaderCell>Status</TableHeaderCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{data.map((item) => (
+									<TableRow key={item.id}>
+										<TableCell>
+											<div className="flex items-center pr-1">
+												<div>
+													<div className="w-10 h-10 mr-3 overflow-hidden rounded-full flex items-center justify-center border border-gray-300">
+														<img
+															src={
+																item?.attributes?.avatar &&
+																item?.attributes?.avatar[0] != ""
+																	? item?.attributes?.avatar[0]
+																	: "/avater.png"
+															}
+															className="w-full h-full"
+														/>
+													</div>
+												</div>
+												<Text className="font-sans">
 													{item.firstName} {item?.lastName}
 												</Text>
-												<Text className="text-lg font-sans">
-													({item.username})
-												</Text>
 											</div>
-											<div className="space-y-2">
-												<div className="h-4 rounded">
-													<Text className="font-sans">{item.email}</Text>
-												</div>
-												<div className="h-4 rounded w-5/6 flex space-x-2">
-													<Text className="font-sans">
-														{item.attributes?.country
-															? item.attributes?.country[0]
-															: ""}
-													</Text>
-													<Text className="font-sans">
-														-{" "}
-														{item.attributes?.gender
-															? item.attributes?.gender[0]
-															: ""}
-													</Text>
-												</div>
-											</div>
-											<div className="space-y-2">
-												<div className="h-4 rounded w-5/6 flex space-x-2">
-													<Text className="font-sans">
-														{item.attributes?.phone
-															? `${
-																	item.attributes?.code
-																		? item.attributes?.code[0]
-																		: ""
-															  }${item.attributes?.phone[0]}`
-															: ""}
-													</Text>
-													<Text className="font-sans">
-														-{" "}
-														{item.enabled ? (
-															<Badge>Active</Badge>
-														) : (
-															<Badge color="slate">Inactive</Badge>
-														)}
-													</Text>
-												</div>
-											</div>
-										</div>
-										<div>
-											<button>
-												<FiMoreVertical className="text-xl" />
-											</button>
-										</div>
-									</div>
-								</Card>
-							</div>
-						))}
-					</div>
-				)}
-			</div>
+										</TableCell>
+										<TableCell>
+											<Text>{item.username}</Text>
+										</TableCell>
+										<TableCell>
+											<Text>{item.email}</Text>
+										</TableCell>
+										<TableCell>
+											<Text>
+												{item.attributes?.phone
+													? `${
+															item.attributes?.code
+																? item.attributes?.code[0]
+																: ""
+													  }${item.attributes?.phone[0]}`
+													: ""}
+											</Text>
+										</TableCell>
+										<TableCell>
+											<Text>
+												{item.attributes?.gender
+													? item.attributes?.gender[0]
+													: "None"}
+											</Text>
+										</TableCell>
+										<TableCell>
+											<Text>
+												{item.attributes?.country
+													? item.attributes?.country[0]
+													: "None"}
+											</Text>
+										</TableCell>
+										<TableCell>
+											{item.emailVerified ? (
+												<Badge
+													className="flex items-center space-x-1"
+													icon={CheckIcon}
+													color="indigo"
+												>
+													True
+												</Badge>
+											) : (
+												<Badge
+													// className="flex items-center"
+													icon={XMarkIcon}
+													color="neutral"
+												>
+													False
+												</Badge>
+											)}{" "}
+										</TableCell>
+										<TableCell>
+											{item.enabled ? (
+												<Badge
+													className="flex items-center space-x-1"
+													color="emerald"
+													icon={WifiIcon}
+												>
+													Active
+												</Badge>
+											) : (
+												<Badge
+													// className="flex items-center"
+													color="neutral"
+													icon={ExclamationCircleIcon}
+												>
+													Inactive
+												</Badge>
+											)}{" "}
+										</TableCell>
+										<TableCell>
+											<Flex>
+												<Button
+													variant="primary"
+													icon={EyeIcon}
+													className="text-white"
+												/>
+												<Button
+													variant="secondary"
+													icon={PencilIcon}
+													className="text-green-500"
+												/>
+												<Button
+													icon={TrashIcon}
+													className="text-white bg-red-500"
+												/>
+											</Flex>
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</Card>
+				</div>
+			</MediaQuery>
 			<button
 				onClick={() => router.push("/users/add")}
 				title="Add User"
-				className="fixed z-90 bottom-10 right-8 w-20 h-20 bg-prim rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-prim hover:drop-shadow-2xl hover:animate-bounce duration-300"
+				className="fixed z-90 bottom-10 right-8 w-16 h-16 md:w-20 md:h-20 lg:h-20 lg:w-20 bg-prim rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-prim hover:drop-shadow-2xl hover:animate-bounce duration-300"
 			>
 				<svg
 					viewBox="0 0 20 20"
