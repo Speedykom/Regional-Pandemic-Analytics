6dd7e387bea2eadb394788a8b072aad9f514e607
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-07-21 14:39:11 +0100

    refactor: user auth redux

--- frontend/src/modules/auth/interface.ts
+++ frontend/src/modules/auth/interface.ts
@@ -20,3 +20,47 @@ export type Credentials = {
   scope: string;
   permissions: Permissions;
 };
+
+
+export interface JwtInfos {
+  realm_access: {
+    roles: string[];
+  };
+  resource_access: Record<
+    string,
+    {
+      roles: string[];
+    }
+  >;
+  email_verified: boolean;
+  preferred_username: string;
+  avatar: string;
+  given_name: string;
+  family_name: string;
+  email: string;
+}
+
+export interface Jwt extends JwtInfos {
+  exp: number;
+  iat: number;
+  jti: string;
+  iss: string;
+  aud: string[];
+  sub: string;
+  typ: string;
+  azp: string;
+  session_state: string;
+  acr: string;
+  'allowed-origins': string[];
+  scope: string;
+  sid: string;
+}
+
+export interface UserProfile extends JwtInfos {
+  id: string;
+  country?: string;
+  gender?: string;
+  phone?: string;
+  code?: string;
+  name?: string;
+}
