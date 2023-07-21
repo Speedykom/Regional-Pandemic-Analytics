033e3b5dbec0122650fb9ba7ed91bc507e75d0ea
Mohamed James Kamara <james.kamara@speedykom.de>
2023-07-21 11:05:31 +0000

    added get user query in redux store and implemented it in user details component

--- frontend/src/modules/user/user.ts
+++ frontend/src/modules/user/user.ts
@@ -1,7 +1,7 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { baseQuery } from '@/common/redux/api';
-import { Users } from './interface';
+import { User, Users } from './interface';
 
 export const UserApi = createApi({
   reducerPath: 'UserApi',
@@ -10,7 +10,10 @@ export const UserApi = createApi({
     getUsers: builder.query<Users, void>({
       query: () => 'account/users',
     }),
+    getUser: builder.query<User, void>({
+      query: (id) => `account/user/${id}`,
+    }),
   }),
 });
 
-export const { useGetUsersQuery } = UserApi;
+export const { useGetUsersQuery, useGetUserQuery } = UserApi;
