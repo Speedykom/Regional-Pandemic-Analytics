298508b7bdcebf2659d458b5467480d48113243f
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-07-21 14:39:11 +0100

    refactor: user auth redux

--- frontend/src/common/components/Dashboard/DashboardFrame.tsx
+++ frontend/src/common/components/Dashboard/DashboardFrame.tsx
@@ -1,11 +1,8 @@
 import Layout from "@/common/components/Dashboard/Layout";
-import { ReactNode, useEffect, useState } from "react";
-import secureLocalStorage from "react-secure-storage";
+import { ReactNode, useEffect } from "react";
 import { useRouter } from "next/router";
-import axios from "axios";
-import Login from "@/pages/index";
-import { Loader } from "../Loader";
-import { Loading } from "../Loading";
+import { useSelector } from "react-redux";
+import { selectCurrentUser, selectIsAuthenticated } from "@/modules/auth/auth";
 
 interface Props {
   title?: string;
@@ -16,45 +13,15 @@ interface Props {
 
 export default function DashboardFrame({ children }: Props) {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const currentUser = useSelector(selectCurrentUser)
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.localStorage) {
-      const authState = secureLocalStorage.getItem("sua") as string;
-      if (authState !== "authenticated") {
-        secureLocalStorage.clear();
-        axios
-          .post("/api/auth/logout/")
-          .then(() => {
-            setIsAuthenticated(false); // Update isAuthenticated state
-            router.push("/");
-          })
-          .catch((error) => {
-            console.log("Logout failed:", error);
-            setIsAuthenticated(false); // Update isAuthenticated state
-            router.push("/");
-          })
-          .finally(() => {
-            setIsLoading(false);
-          });
-      } else {
-        setIsAuthenticated(true); // Update isAuthenticated state if authenticated
-        setIsLoading(false);
-      }
-    } else {
-      setIsLoading(false);
+    if (!isAuthenticated) {
+      router.push("/");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  if (isLoading) {
-    return <Loading />
-  }
-
-  if (!isAuthenticated) {
-    return <Login />; // Redirect to Login component if not authenticated
-  }
 
   return (
     <Layout>
