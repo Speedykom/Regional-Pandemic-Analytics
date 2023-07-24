e627bdd28a6ce7856c854f5729867b5c3054b9c9
Mohamed James Kamara <james.kamara@speedykom.de>
2023-07-24 08:27:21 +0000

    manage client roles instead of realm, role list refactor, role update redux mutation, tremor update fix errors

---
+++ frontend/src/modules/roles/role.ts
@@ -0,0 +1,25 @@
+import { baseQuery } from "@/common/redux/api";
+import { createApi } from "@reduxjs/toolkit/dist/query/react";
+import { Role, Roles } from "./interface";
+
+export const RoleApi = createApi({
+  reducerPath: 'RoleApi',
+  baseQuery,
+  endpoints: builder => ({
+    getRoles: builder.query<Roles, void>({
+      query: () => '/role',
+    }),
+    updateRole: builder.mutation<{message: string}, Pick<Role, 'id' | 'name' | 'description'>> ({
+      query: ({ id, ...patch }) => ({
+        url: `/role/${id}`,
+        method: 'PUT',
+        body: patch
+      })
+    })
+  })
+});
+
+export const {
+  useGetRolesQuery,
+  useUpdateRoleMutation
+} = RoleApi;
