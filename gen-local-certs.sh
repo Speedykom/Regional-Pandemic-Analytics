da244a8243d0ad32f8b956a594ec121b5a770cee
Thorsten Schoel <schoel@gfa-bis.de>
2023-07-28 14:49:06 +0200

    fix: ensure local certificates have correct subject

--- gen-local-certs.sh
+++ gen-local-certs.sh
@@ -14,11 +14,24 @@ CA=myCA.pem
 commonname=myCA
 if test -f "$CA"; then
     echo "$FILE already exists."
-else 
+else
     openssl genrsa -out myCA.key 2048
     openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1825 -out myCA.pem \
          -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/"
     echo files generated!
+
+    if command -v sudo 2&>1 >> /dev/null && command -v trust 2&>1 >> /dev/null; then
+        echo -n "Installing local CA certificate to system trust store… "
+        sudo trust anchor ./myCA.pem
+        echo "done"
+    else
+        echo ********************************************************************************
+        echo * Please make sure to install the local CA root certificate into your          *
+        echo * operating system’s trust store on many Linux systems this is done with       *
+        echo * `trust anchor myCA.pem`. Please Consult the documentation of your OS on how  *
+        echo * to achieve this on your computer.                                            *
+        echo ********************************************************************************
+    fi
 fi
 
 #Our services
@@ -29,7 +42,7 @@ commonname=$service
 mkdir $service.igad.local
 openssl genrsa -out $service.igad.local/privkey.pem 2048
 openssl req -new -key $service.igad.local/privkey.pem -out $service.igad.local/$service.csr \
-    -subj "/C=$country/1=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/"
+    -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/"
 
 cat > $service.igad.local/$service.ext << EOF
 authorityKeyIdentifier=keyid,issuer
@@ -37,7 +50,7 @@ basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
 subjectAltName = @alt_names
 [alt_names]
-DNS.1 = $service
+DNS.1 = $service.igad.local
 EOF
 
 openssl x509 -req -in $service.igad.local/$service.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial \
