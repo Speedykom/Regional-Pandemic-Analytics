7090253697f46633495f7b23455b0c81a5cd246e
sihamouda <anis.c.hamouda@gmail.com>
2023-08-01 16:38:32 +0100

    feat: create minio buckets on startup

---
+++ minio/setup.sh
@@ -0,0 +1,24 @@
+get_bucket(){
+    # $1 for the alias 
+    # $2 for the name of the bucket 
+
+    # checks if the bucket exists 
+    isCreated=$(mc ls $1 | grep $2)
+    
+    # if it doesn't exist
+    if [ -z "$isCreated" ]
+    then 
+        # it creates the bucket
+        mc mb "$1/$2"
+    else
+        echo "$1/$2 exists!"
+    fi
+    
+}
+
+# create an alias to connect to minio container 
+mc alias set minio-remote $MINIO_REMOTE_SERVER_URL $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+
+# create minio buckets 
+get_bucket "minio-remote" "pipelines"
+get_bucket "minio-remote" "repan-bucket"
