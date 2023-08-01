7bb3c1b51e3f602d415d9f96f402cc917adda509
Mohamed James Kamara <james.kamara@speedykom.de>
2023-08-01 11:13:43 +0000

    partial custom drawer

--- frontend/src/common/components/Dashboard/Layout.tsx
+++ frontend/src/common/components/Dashboard/Layout.tsx
@@ -1,14 +1,15 @@
 import { useState, useEffect, Fragment, ReactNode } from "react";
 import SideBar from "./SideBar";
 import TopBar from "./TopBar";
+import Drawer from "../common/Drawer";
 
 interface props {
 	children: ReactNode;
 }
 
 export default function Layout({ children }: props) {
-	const [showNav, setShowNav] = useState(true);
 	const [isMobile, setIsMobile] = useState(false);
+	const [showMobileNav, setShowMobileNav] = useState(false);
 
 	function handleResize() {
 		if (innerWidth <= 640) {
@@ -30,15 +31,21 @@ export default function Layout({ children }: props) {
 
 	return (
 		<>
-			<TopBar />
+			<TopBar
+				isMobile={isMobile}
+				isOpen={showMobileNav}
+				setIsOpen={setShowMobileNav}
+			/>
 			<main className="flex">
-					<div className={`${isMobile && "hidden"} mt-16`}>
-						<SideBar />
-						{/* <SideBar open={showNav} setOpen={setShowNav} /> */}
-					</div>
-				<div
-					className={`w-full pt-16 transition-all duration-[400ms]`}
-				>
+				<Drawer isOpen={showMobileNav} setIsOpen={setShowMobileNav}>
+					<p>Hello</p>
+				</Drawer>
+
+				<div className={`${isMobile && "hidden"} mt-16`}>
+					<SideBar />
+					{/* <SideBar open={showNav} setOpen={setShowNav} /> */}
+				</div>
+				<div className={`w-full pt-16 transition-all duration-[400ms]`}>
 					<div className="px-4 bg-gray-100 h-screen">{children}</div>
 				</div>
 			</main>
