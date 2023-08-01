1cf157811375003fddf21bac4bfd3b485840639e
Mohamed James Kamara <james.kamara@speedykom.de>
2023-08-01 17:52:29 +0000

    sidebar responsive with new design done

--- frontend/src/common/components/Dashboard/Layout.tsx
+++ frontend/src/common/components/Dashboard/Layout.tsx
@@ -1,58 +1,52 @@
-import { useState, useEffect, Fragment, ReactNode } from "react";
+import { useState, ReactNode } from "react";
 import SideBar from "./SideBar";
 import TopBar from "./TopBar";
 import Drawer from "../common/Drawer";
+import { SideNavLinks } from "./Menu";
+import { useMediaQuery } from "react-responsive";
 
 interface props {
 	children: ReactNode;
 }
 
 export default function Layout({ children }: props) {
-	const [isMobile, setIsMobile] = useState(false);
 	const [showMobileNav, setShowMobileNav] = useState(false);
-
-	function handleResize() {
-		if (innerWidth <= 640) {
-			setIsMobile(true);
-		} else {
-			setIsMobile(false);
-		}
-	}
-
-	useEffect(() => {
-		if (typeof window != undefined) {
-			addEventListener("resize", handleResize);
-		}
-
-		return () => {
-			removeEventListener("resize", handleResize);
-		};
-	}, []);
+	const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1024px)" });
 
 	return (
 		<>
 			<TopBar
-				isMobile={isMobile}
 				isOpen={showMobileNav}
 				setIsOpen={setShowMobileNav}
+				isTabletOrMobile={isTabletOrMobile}
 			/>
 			<main className="flex">
-				<Drawer isOpen={showMobileNav} setIsOpen={setShowMobileNav}>
-					<p>Hello</p>
-				</Drawer>
-
-				<div className={`${isMobile && "hidden"} mt-16`}>
-					<SideBar />
-					{/* <SideBar open={showNav} setOpen={setShowNav} /> */}
+				<div className={`mt-16`}>
+					{!isTabletOrMobile && <SideBar />}
+					<Drawer
+						title=""
+						placement="left"
+						isOpen={showMobileNav}
+						onClose={() => setShowMobileNav(false)}
+					>
+						<div className="px-4 py-4">
+							<div className={`text-gray-500`}>
+								<div className="px-4 flex items-center justify-center flex-col mx-4 my-auto">
+									<img
+										className={`w-32 h-auto`}
+										src="/images/igad_logo.jpeg"
+										alt="company-logo"
+									/>
+								</div>
+								<SideNavLinks />
+							</div>
+						</div>
+					</Drawer>
 				</div>
 				<div className={`w-full pt-16 transition-all duration-[400ms]`}>
-					<div className="px-4 bg-gray-100 h-screen">{children}</div>
+					<div className="bg-gray-100 h-screen">{children}</div>
 				</div>
 			</main>
 		</>
 	);
 }
-
-// ${
-// 	showNav ? "pl-56" : ""
-// }
