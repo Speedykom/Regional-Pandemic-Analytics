2af3a24168744d0c22348e2b41274af9b7f32ee3
zbidi <hamza@speedykom.de>
2023-08-01 14:05:36 +0100

    refactor: use python-keycloak admin client

--- backend/roles/views.py
+++ backend/roles/views.py
@@ -4,9 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from utils.keycloak_auth import keycloak_admin_login
-from utils.env_configs import (
-    APP_REALM, APP_USER_ROLES, BASE_URL)
+from utils.keycloak_auth import get_keycloak_admin
+from django.conf import settings
 
 #Api to create and list all roles
 class RoleApiView(APIView):
@@ -15,33 +14,14 @@ class RoleApiView(APIView):
     """_api view to get client roles_
     """
     def get(self, request, *args, **kwargs):
-        # Login to admin
-        admin_login = keycloak_admin_login()
-        
-        if admin_login["status"] != 200:
-            return Response(admin_login["data"], status=admin_login["status"])
+        try:
+            keycloak_admin = get_keycloak_admin()
+            client_id = keycloak_admin.get_client_id(settings.KEYCLOAK_CONFIG['KEYCLOAK_CLIENT_ID'])
+            client_roles = keycloak_admin.get_client_roles(client_id=client_id)
+            return Response(client_roles, status=status.HTTP_200_OK)
+        except Exception as err:
+            return Response({'errorMessage': 'Unable to retrieve the roles'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        headers = {
-            'Authorization': f"Bearer {admin_login['data']['access_token']}",
-            'Content-Type': "application/json",
-            'cache-control': "no-cache"
-        }
-        
-        id_response = requests.get(f"{BASE_URL}/admin/realms/{APP_REALM}/clients?clientId={os.getenv('CLIENT_ID')}", headers=headers)
-        
-        if not id_response.ok:
-            return Response(id_response.reason, status=id_response.status_code)
-        
-        client_uuid = id_response.json()[0]['id']
-
-        response = requests.get(f"{BASE_URL}/admin/realms/{APP_REALM}/clients/{client_uuid}/roles", headers=headers)
-
-        if not response.ok:
-            return Response(response.reason, status=response.status_code)
-
-        role_data = response.json()
-        return Response(role_data, status=status.HTTP_200_OK)
-    
     """
     API view to update Keycloak client role
     """
@@ -49,71 +29,14 @@ class RoleApiView(APIView):
         form_data = {
             "name": request.data.get("name", None),
             "description": request.data.get("description", None),
-        } 
-
-        # Login to admin
-        admin_login = keycloak_admin_login()
-        
-        if admin_login["status"] != 200:
-            return Response(admin_login["data"], status=admin_login["status"])
-
-        headers = {
-            'Authorization': f"{admin_login['data']['token_type']} {admin_login['data']['access_token']}",
-            'Content-Type': "application/json",
-            'cache-control': "no-cache"
         }
-        
-        id_response = requests.get(f"{BASE_URL}/admin/realms/{APP_REALM}/clients?clientId={os.getenv('CLIENT_ID')}", headers=headers)
-        
-        if not id_response.ok:
-            return Response(id_response.reason, status=id_response.status_code)
-        
-        client_uuid = id_response.json()[0]['id']
 
-        res = requests.put(
-            f"{BASE_URL}/admin/realms/{APP_REALM}/clients/{client_uuid}/roles/{kwargs['id']}", json=form_data, headers=headers)
+        try:
+            keycloak_admin = get_keycloak_admin()
+            client_id = keycloak_admin.get_client_id(settings.KEYCLOAK_CONFIG['KEYCLOAK_CLIENT_ID'])
+            keycloak_admin.update_client_role(client_role_id=client_id, role_name=kwargs['name'], payload=form_data)
+            return Response({'message': 'Role update was successful'}, status=status.HTTP_200_OK)
+        except Exception as err:
+            print(err)
+            return Response({'errorMessage': 'Unable to update the role'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        if not res.ok:
-            return Response(res.reason, status=res.status_code)
-
-        return Response({'message': 'Role update was successful'}, status=status.HTTP_200_OK)    
-
-class GetEditRole(APIView):
-    permission_classes = [AllowAny, ]
-    
-    def get(self, request, *args, **kwargs):
-        # Login to admin
-        admin_login = keycloak_admin_login()
-
-        if admin_login["status"] != 200:
-            return Response(admin_login["data"], status=admin_login["status"])
-
-        headers = {
-            'Authorization': f"Bearer {admin_login['data']['access_token']}",
-            'Content-Type': "application/json",
-            'cache-control': "no-cache"
-        }
-        
-        response: any
-
-        type = request.query_params['type']
-
-        if type == None:
-            return Response({'errorMessage': 'Query parameter `type` is required'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        elif type == 'id':
-            response = requests.get(
-                f"{BASE_URL}/admin/realms/{APP_REALM}/roles-by-id/{kwargs['id']}", headers=headers)
-
-        elif type == 'name':
-            response = requests.get(f"{APP_USER_ROLES}/{kwargs['id']}", headers=headers)
-        
-        elif type != 'name' or type != 'id':
-            return Response({'errorMessage': 'query param type must either be `name` or `id`'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if response.status_code != 200:
-            return Response(response.reason, status=response.status_code)
-        
-        role = response.json()
-        return Response({'status': 'success', 'role': role}, status=status.HTTP_200_OK)
-      
