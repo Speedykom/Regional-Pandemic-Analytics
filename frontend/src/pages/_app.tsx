23ecb7c5e6ef2e4c87c2480dd5f5c937ca611a64
Mohamed James Kamara <james.kamara@speedykom.de>
2023-08-09 09:22:36 +0000

    fix template modal

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,47 +1,47 @@
-import { ToastContainer } from 'react-toastify';
-import { SWRConfig } from 'swr';
-import axios from 'axios';
-import { AppProps } from 'next/app';
-import '@/common/styles/globals.css';
-import 'react-toastify/dist/ReactToastify.css';
-import { Provider } from 'react-redux';
-import { ConfigProvider } from 'antd';
-import { store } from '@/common/redux/store';
-import { ModalProvider } from '@/common/hooks/use-modal';
-import dynamic from 'next/dynamic';
+import { ToastContainer } from "react-toastify";
+import { SWRConfig } from "swr";
+import axios from "axios";
+import { AppProps } from "next/app";
+import "@/common/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux";
+import { ConfigProvider } from "antd";
+import { store } from "@/common/redux/store";
+import { ModalProvider } from "@/common/hooks/use-modal";
+import dynamic from "next/dynamic";
 
 function CsrApp({ Component, pageProps }: AppProps) {
-  return (
-    <SWRConfig
-      value={{
-        refreshInterval: 3000,
-        fetcher: (resource, init) =>
-          axios.get(resource, init).then((res) => res.data),
-      }}
-    >
-      <ModalProvider>
-        <Provider store={store}>
-          <ConfigProvider
-            theme={{
-              components: {},
-              token: {
-                colorPrimary: '#007b38',
-                fontSize: 14,
-              },
-            }}
-          >
-            <Component {...pageProps} />
-            <ToastContainer />
-          </ConfigProvider>
-        </Provider>
-      </ModalProvider>
-    </SWRConfig>
-  );
+	return (
+		<SWRConfig
+			value={{
+				refreshInterval: 3000,
+				fetcher: (resource, init) =>
+					axios.get(resource, init).then((res) => res.data),
+			}}
+		>
+			<Provider store={store}>
+				<ModalProvider>
+					<ConfigProvider
+						theme={{
+							components: {},
+							token: {
+								colorPrimary: "#007b38",
+								fontSize: 14,
+							},
+						}}
+					>
+						<Component {...pageProps} />
+						<ToastContainer />
+					</ConfigProvider>
+				</ModalProvider>
+			</Provider>
+		</SWRConfig>
+	);
 }
 
 // We are disabling the SSR for the whole app since the rendering
 // depends mainly on the current user status (logged in or not)
-// Since the user token is stored in the browser local storage, the CSR / SSR rendering 
+// Since the user token is stored in the browser local storage, the CSR / SSR rendering
 // will be different.
 //
 // See more info here: https://nextjs.org/docs/messages/react-hydration-error
