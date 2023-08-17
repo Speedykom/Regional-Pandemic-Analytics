ef1679247304942da408ef9580465376e6dda4d4
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-08-17 18:27:48 +0100

    fix: frontend websocket hotreload + Hop back btn

--- frontend/src/modules/pipeline/views/HopUI.tsx
+++ frontend/src/modules/pipeline/views/HopUI.tsx
@@ -1,3 +1,6 @@
+import { ChevronLeftIcon } from "@heroicons/react/20/solid";
+import { Button } from "@tremor/react";
+import { useRouter } from "next/router";
 import { useGetPipelineQuery } from "../pipeline";
 
 interface HopUIProps {
@@ -6,12 +9,22 @@ interface HopUIProps {
 
 export const HopUI = ({ id }: HopUIProps) => {
 	const { data } = useGetPipelineQuery(id);
+	const router = useRouter();
 	return (
 		<div className="">
 			<nav className="mb-5 flex justify-between items-center">
 				<div>
 					<h2 className="text-3xl">Pipeline : {data?.name}</h2>
 					<p className="my-2 text-gray-600">{data?.description}</p>
+				</div>
+				<div>
+					<Button
+						icon={ChevronLeftIcon}
+						onClick={() => router.push(`/pipelines`)}
+						className="bg-prim hover:bg-prim-hover border-0"
+					>
+						My Pipelines
+					</Button>
 				</div>
 			</nav>
 			<div>		
