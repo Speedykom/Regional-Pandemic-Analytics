89feda5d3fb8e7e5ed2551440e2ef03672a3e41b
Mohamed Marrouchi <marrouchi.mohamed@gmail.com>
2023-08-18 10:21:30 +0100

    fix: session end/expiry + redirect

--- frontend/src/pages/pipelines/[id].tsx
+++ frontend/src/pages/pipelines/[id].tsx
@@ -1,5 +1,5 @@
 import { Unauthorized } from "@/common/components/common/unauth";
-import DashboardFrame from "@/common/components/Dashboard/DashboardFrame";
+import Layout from "@/common/components/Dashboard/Layout";
 import { usePermission } from "@/common/hooks/use-permission";
 import { HopUI } from "@/modules/pipeline/views/HopUI";
 import { useRouter } from "next/router";
@@ -9,8 +9,8 @@ export default function Pipeline() {
 	const { hasPermission } = usePermission();
 	const { id } = router.query as { id: string };
 	return (
-		<DashboardFrame title="My Pipeline">
+		<Layout>
 			{hasPermission("pipeline:read") && id ? <HopUI id={id} /> : <Unauthorized />}
-		</DashboardFrame>
+		</Layout>
 	);
 }
