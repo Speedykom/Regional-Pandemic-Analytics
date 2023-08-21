c174fe185c6cae7bae3bdfb4ffb672d85640c8cf
sihamouda <anis.c.hamouda@gmail.com>
2023-08-21 16:07:27 +0100

    feat: automate template upload

--- minio/setup.sh
+++ minio/setup.sh
@@ -1,25 +1,31 @@
 create_bucket(){
-    # $1 for the alias 
-    # $2 for the name of the bucket 
+    # $1 for the name of the bucket 
 
     # checks if the bucket exists 
-    isCreated=$(mc ls $1 | grep $2)
+    isCreated=$(aws --endpoint-url $MINIO_ENDPOINT_URL s3 ls | grep $1)
     
     # if it doesn't exist
     if [ -z "$isCreated" ]
     then 
         # it creates the bucket
-        mc mb "$1/$2"
+        aws --endpoint-url $MINIO_ENDPOINT_URL s3 mb s3://"$1"
     else
-        echo "$1/$2 exists!"
+        echo "$1 exists!"
     fi
     
 }
 
-# create an alias to connect to minio container 
-mc alias set minio-remote $MINIO_MC_REMOTE_SERVER_URL $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+# configure minio access key, secret key, region and s3 signature
+aws configure set profile.default.aws_access_key_id $MINIO_ACCESS_KEY
+aws configure set profile.default.aws_secret_access_key $MINIO_SECRET_KEY
+# aws configure set profile.default.region $MINIO_REGION
+aws configure set profile.default.s3.signature_version s3v4
 
 # create minio buckets 
-create_bucket "minio-remote" "pipelines"
-create_bucket "minio-remote" "repan-bucket"
-create_bucket "minio-remote" "avatars"
+create_bucket "pipelines"
+create_bucket "parquets"
+create_bucket "repan-bucket"
+create_bucket "avatars"
+
+# upload templates
+aws --endpoint-url $MINIO_ENDPOINT_URL s3 sync /aws/templates/ s3://pipelines/templates/
