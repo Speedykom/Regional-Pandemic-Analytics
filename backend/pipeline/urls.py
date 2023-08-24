ffbc31121fbf61f9c9cc338360ed6d2ef2297b60
zbidi <hamza@speedykom.de>
2023-08-24 09:07:44 +0100

    refactor: create pipeline app & refactor api urls import

---
+++ backend/pipeline/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import PipelineListView, PipelineDetailView
+
+urlpatterns = [
+    path("", PipelineListView.as_view()),
+    path("/list", PipelineListView.as_view()),
+    path("/<str:name>", PipelineDetailView.as_view()),
+]
