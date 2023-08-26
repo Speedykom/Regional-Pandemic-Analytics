b17998541a74d607096c98fd10ec17b0c9a3fda8
sihamouda <anis.c.hamouda@gmail.com>
2023-08-26 22:22:13 +0100

    refactor frontend - 1

--- backend/process/urls.py
+++ backend/process/urls.py
@@ -4,11 +4,11 @@ from .views import ProcessView, ProcessRunView
 urlpatterns = [
     path("", ProcessView.as_view({"get": "list", "post": "create"})),
     path(
-        "<str:dag_id>",
+        "/<str:dag_id>",
         ProcessView.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}),
     ),
     path(
-        "<str:dag_id>/dagRuns",
+        "/<str:dag_id>/dagRuns",
         ProcessRunView.as_view({"get": "list", "post": "create"}),
     ),
 ]
