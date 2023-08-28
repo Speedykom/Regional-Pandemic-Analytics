6a81561b3527118f74f9c0fa3540144db3cf33c0
vboxuser <vboxuser@ubu.myguest.virtualbox.org>
2023-08-28 19:21:33 +0200

    footer part

---
+++ frontend/src/common/components/BusinessAgreement.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const BusinessAgreement: React.FC = () => {
+  return (
+    <div className="footerSection">
+      <h2 className="sectionTitle">Business Agreement</h2>
+      <p className="sectionText">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+      </p>
+    </div>
+  );
+};
+
+export default BusinessAgreement;
---
+++ frontend/src/common/components/Privacy.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const Privacy: React.FC = () => {
+  return (
+    <div className="footerSection">
+      <h2 className="sectionTitle">Privacy</h2>
+      <p className="sectionText">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
+
