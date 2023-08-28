1df27861caedb39233eee372e29a54d502df6154
vboxuser <vboxuser@ubu.myguest.virtualbox.org>
2023-08-28 19:21:33 +0200

    footer part

---
+++ frontend/src/common/components/Footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <ul>
+        <li>
+          <Link href="/learn-more">Learn More about RePan</Link>
+        </li>
+        <li>
+          <Link href="/privacy">Privacy</Link>
+        </li>
+        <li>
+          <Link href="/terms-of-service">Terms of Service</Link>
+        </li>
+        <li>
+          <Link href="/business-agreement">Business Agreement</Link>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
+
+
