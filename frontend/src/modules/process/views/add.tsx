efbc7ab3a65d4a08ddc00eda792808f46a7f805c
sihamouda <anis.c.hamouda@gmail.com>
2023-08-29 16:06:15 +0100

    front refactor - 2

--- frontend/src/modules/process/views/add.tsx
+++ frontend/src/modules/process/views/add.tsx
@@ -4,99 +4,100 @@ import { schedule_intervals } from '@/common/utils/processs';
 import { useGetAllPipelinesQuery } from '@/modules/pipeline/pipeline';
 import { Button, Form, Input, Select } from 'antd';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { DagForm } from '../interface';
 
-interface Props {
-  state: boolean;
-  onClose: () => void;
-}
+export const AddProcess = ({ panelState, closePanel }: any) => {
+  const { register, handleSubmit } = useForm();
+  const {
+    data: pipelineList,
+    // isLoading,
+    isSuccess,
+  } = useGetAllPipelinesQuery();
 
-export const AddProcess = ({ state, onClose }: Props) => {
-  const [form] = Form.useForm();
-  const [loading, setLoading] = useState(false);
-  // const [addProcess] = useCreateProcessChainMutation();
+    const footer = (
+      <div className="space-x-2 p-2">
+        <Button
+          className="bg-prim text-white border-0 hover:bg-prim-hover"
+          onClick={handleSubmit(async (values) => {
+            await createProcess({
+              name: values.processName,
+              pipeline: values.pipelineTemplate,
+              schedule_interval: values.scheduleInterval,
+            } as DagForm);
+            closePanel();
+          })}
+        >
+          Submit
+        </Button>
+        <Button
+          className="bg-blue-100 px-4 py-2 text-sm text-blue-900 hover:bg-blue-200 border-0"
+          onClick={closePanel}
+        >
+          Cancel
+        </Button>
+      </div>
+    );
 
-  const { data: res } = useGetAllPipelinesQuery();
-
-  const pipelines = res?.data || [];
-
-  const onFinish = (value: any) => {
-    setLoading(true);
-  };
-
-  const cancel = () => {
-    form.resetFields();
-    onClose();
-  };
-
-  const footer = (
-    <div className="space-x-2 p-2">
-      <Button loading={loading} type="primary" onClick={() => form.submit()}>
-        Submit
-      </Button>
-      <Button onClick={cancel} type="default">
-        Cancel
-      </Button>
-    </div>
-  );
-
-  return (
-    <AppDrawer
-      title="Add Process Chain"
-      state={state}
-      onClose={cancel}
-      footer={footer}
-    >
-      <Form
-        form={form}
-        name="add-process"
-        onFinish={onFinish}
-        layout="vertical"
-        scrollToFirstError
+    return (
+      <AppDrawer
+        title="Add Process Chain"
+        state={state}
+        onClose={cancel}
+        footer={footer}
       >
-        <Form.Item
-          name="name"
-          label="Name"
-          tooltip="Process Name"
-          rules={[
-            {
-              required: true,
-              message: 'Please enter process name',
-            },
-          ]}
+        <Form
+          form={form}
+          name="add-process"
+          onFinish={onFinish}
+          layout="vertical"
+          scrollToFirstError
         >
-          <Input placeholder="Enter Dag Name" className="w-full" />
-        </Form.Item>
-        <Form.Item name="pipeline" label="Pipeline" className="w-full">
-          <Select
-            showSearch
-            placeholder="Select Pipeline"
-            options={pipelines.map((pipeline: any) => ({
-              value: `${pipeline.name}.hpl`,
-              label: pipeline.name,
-            }))}
-          />
-        </Form.Item>
-        <Form.Item
-          name="schedule_interval"
-          label="Schedule Interval"
-          rules={[
-            {
-              required: true,
-              message: 'Please select schedule interval',
-            },
-          ]}
-        >
-          <Select
-            showSearch
-            placeholder="Select Schedule Interval"
-            options={schedule_intervals.map((time: string, i: number) => ({
-              key: i,
-              label: time,
-              value: time,
-            }))}
-          />
-        </Form.Item>
-      </Form>
-    </AppDrawer>
-  );
+          <Form.Item
+            name="name"
+            label="Name"
+            tooltip="Process Name"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter process name',
+              },
+            ]}
+          >
+            <Input placeholder="Enter Dag Name" className="w-full" />
+          </Form.Item>
+          <Form.Item name="pipeline" label="Pipeline" className="w-full">
+            <Select
+              showSearch
+              placeholder="Select Pipeline"
+              options={pipelines.map((pipeline: any) => ({
+                value: `${pipeline.name}.hpl`,
+                label: pipeline.name,
+              }))}
+            />
+          </Form.Item>
+          <Form.Item
+            name="schedule_interval"
+            label="Schedule Interval"
+            rules={[
+              {
+                required: true,
+                message: 'Please select schedule interval',
+              },
+            ]}
+          >
+            <Select
+              showSearch
+              placeholder="Select Schedule Interval"
+              options={schedule_intervals.map((time: string, i: number) => ({
+                key: i,
+                label: time,
+                value: time,
+              }))}
+            />
+          </Form.Item>
+        </Form>
+      </AppDrawer>
+    );
+  };
 };
