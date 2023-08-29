86aa3dc72a649cc52d471d6032d618baf30159ee
Lindanajar <Lindanajar@github.com>
2023-08-29 11:19:44 +0200

    add footer component

--- frontend/src/common/components/Footer.tsx
+++ frontend/src/common/components/Footer.tsx
@@ -2,26 +2,35 @@ import React from 'react';
 import Link from 'next/link';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+  
   return (
-    <footer className="footer">
-      <ul>
-        <li>
-          <Link href="/learn-more">Learn More about RePan</Link>
-        </li>
-        <li>
-          <Link href="/privacy">Privacy</Link>
-        </li>
-        <li>
-          <Link href="/terms-of-service">Terms of Service</Link>
-        </li>
-        <li>
-          <Link href="/business-agreement">Business Agreement</Link>
-        </li>
-      </ul>
+    <footer className="bg-white py-8">
+      <div className="container mx-auto max-w-screen-lg"> 
+        <div className="px-4 flex flex-col md:flex-row items-center justify-between text-gray-600">
+          <div className="mb-4 md:mb-0 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+            <ul className="mb-4 md:mb-0 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+              <li>
+                <Link href="footer/learn-more" className="text-prim">Learn More</Link>
+              </li>
+              <li>
+                <Link href="footer/privacy" className="text-prim">Privacy</Link>
+              </li>
+              <li>
+                <Link href="footer/terms-of-service" className="text-prim">Terms of Service</Link>
+              </li>
+              <li>
+                <Link href="footer/business-agreement" className="text-prim">Business Agreement</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="text-sm text-gray-500">
+            &copy; {currentYear} Repan. All Rights Reserved.
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
-
-
--- frontend/src/pages/footer/learn-more.tsx
+++ frontend/src/pages/footer/learn-more.tsx
@@ -4,10 +4,9 @@ import LearnMore from '../../common/components/LearnMore';
 
 const LearnMorePage: React.FC = () => {
   return (
-    <div className="footerSection">
-      <h2 className="sectionTitle">Learn More about RePan</h2>
+    
       <LearnMore />
-    </div>
+    
   );
 };
 
