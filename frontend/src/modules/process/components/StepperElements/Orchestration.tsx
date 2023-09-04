c8a2882a1f2f40cd96810a6bab04614885dfaf54
zbidi <hamza@speedykom.de>
2023-09-04 09:43:10 +0100

    orchestration layout

--- frontend/src/modules/process/components/StepperElements/Orchestration.tsx
+++ frontend/src/modules/process/components/StepperElements/Orchestration.tsx
@@ -1,4 +1,10 @@
-import { Table, TableCell, TableHeaderCell, TableRow } from '@tremor/react';
+import {
+  Table,
+  TableCell,
+  TableHeaderCell,
+  TableRow,
+  Divider,
+} from '@tremor/react';
 import History from './History';
 
 interface OrchestrationProps {
@@ -15,26 +21,34 @@ export default function Orchestration({
   nextDagRun,
 }: OrchestrationProps) {
   return (
-    <div className="flex space-y-3">
-      <Table className="flex justify-center overflow-visible w-1/2">
-        <TableRow>
-          <TableHeaderCell>description</TableHeaderCell>
-          <TableCell className="whitespace-normal">
-            {description} hello there this is a basic description element that
-            should be working. By the way, have fun in the demo. There is no
-            description field in the Add component so please remember to add it
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableHeaderCell>Last update</TableHeaderCell>
-          <TableCell className="whitespace-normal">{lastParsedTime}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableHeaderCell>Next scheduled execution</TableHeaderCell>
-          <TableCell className="whitespace-normal">{nextDagRun}</TableCell>
-        </TableRow>
-      </Table>
-      <div className="grow">
+    <div className="flex flex-col space-y-3 ">
+      <div className="flex justify-center">
+        <Table className="flex justify-center overflow-visible w-1/2">
+          <TableRow>
+            <TableHeaderCell>description</TableHeaderCell>
+            <TableCell className="whitespace-normal">
+              {description} hello there this is a basic description element that
+              should be working. By the way, have fun in the demo. There is no
+              description field in the Add component so please remember to add
+              it
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableHeaderCell>Last update</TableHeaderCell>
+            <TableCell className="whitespace-normal">
+              {lastParsedTime}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableHeaderCell>Next scheduled execution</TableHeaderCell>
+            <TableCell className="whitespace-normal">{nextDagRun}</TableCell>
+          </TableRow>
+        </Table>
+      </div>
+      <div className="">
+        <Divider />
+      </div>
+      <div>
         <History dagId={dagId} />
       </div>
     </div>
