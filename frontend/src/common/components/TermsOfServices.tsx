8bcd853fa279fc216de3a5a9c0bd5ba4eba27787
Lindanajar <Lindanajar@github.com>
2023-09-06 11:29:36 +0200

    using tailwind css

--- frontend/src/common/components/TermsOfServices.tsx
+++ frontend/src/common/components/TermsOfServices.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const TermsOfService: React.FC = () => {
   return (
-    <div className="footerSection">
-      <h2 className="sectionTitle">Terms of Service</h2>
-      <p className="sectionText">
+    <div className="p-4 bg-gray-200 border border-gray-300 rounded">
+      <h2 className="text-xl font-bold text-green-600 mb-2">
+        Terms of Service
+      </h2>
+      <p className="text-base text-gray-700">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit...
       </p>
     </div>
