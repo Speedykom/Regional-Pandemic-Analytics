fa9e9e5e2182335de89f205f7296b33d16923930
Lindanajar <Lindanajar@github.com>
2023-09-06 11:29:36 +0200

    using tailwind css

--- frontend/src/common/components/LearnMore.tsx
+++ frontend/src/common/components/LearnMore.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const LearnMore: React.FC = () => {
   return (
-    <div className="footerSection">
-      <h2 className="sectionTitle">Learn More about RePan</h2>
-      <p className="sectionText">
+    <div className="p-4 bg-gray-200 border border-gray-300 rounded">
+      <h2 className="text-xl font-bold text-green-600 mb-2">
+        Learn More about RePan
+      </h2>
+      <p className="text-base text-gray-700">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit...
       </p>
     </div>
