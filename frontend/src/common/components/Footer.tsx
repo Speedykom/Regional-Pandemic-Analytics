14f4fea8049a8a3b7d0c458809128a5ad00c3240
Lindanajar <Lindanajar@github.com>
2023-09-06 12:13:11 +0200

    fixing tailwind css in footer files

--- frontend/src/common/components/Footer.tsx
+++ frontend/src/common/components/Footer.tsx
@@ -5,31 +5,28 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-100 py-8">
+    <footer className="bg-white py-8">
       <div className="container mx-auto max-w-screen-lg">
         <div className="px-4 flex flex-col md:flex-row items-center justify-between text-gray-600">
           <div className="mb-4 md:mb-0 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
             <ul className="mb-4 md:mb-0 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
               <li>
-                <Link href="footer/learn-more" className="text-green-600">
+                <Link href="/footer/learn-more" className="text-prim">
                   Learn More
                 </Link>
               </li>
               <li>
-                <Link href="footer/privacy" className="text-green-600">
+                <Link href="/footer/privacy" className="text-prim">
                   Privacy
                 </Link>
               </li>
               <li>
-                <Link href="footer/terms-of-service" className="text-green-600">
+                <Link href="/footer/terms-of-service" className="text-prim">
                   Terms of Service
                 </Link>
               </li>
               <li>
-                <Link
-                  href="footer/business-agreement"
-                  className="text-green-600"
-                >
+                <Link href="/footer/business-agreement" className="text-prim">
                   Business Agreement
                 </Link>
               </li>
