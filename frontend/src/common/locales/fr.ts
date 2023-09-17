7e74e04839a45c1017af6c0c3af6bbb2cf7b5819
Lindanajar <Lindanajar@github.com>
2023-09-17 14:33:47 +0200

    lang files

---
+++ frontend/src/common/locales/fr.ts
@@ -0,0 +1,26 @@
+const frTranslation = {
+  appAccounts: "Comptes de l'application",
+  viewAndManage:
+    "Afficher et gérer les paramètres liés aux utilisateurs de l'application.",
+  newUser: 'Nouvel utilisateur',
+  fullName: 'Nom complet',
+  username: "Nom d'utilisateur",
+  email: 'E-mail',
+  phone: 'Téléphone',
+  gender: 'Genre',
+  country: 'Pays',
+  emailVerified: 'E-mail vérifié',
+  status: 'Statut',
+  viewDetails: 'Voir les détails',
+  disableUser: "Désactiver l'utilisateur",
+  deleteUserConfirmation:
+    "Cet utilisateur se verra refuser l'accès, continuer ?",
+  myPipelines: 'Mes pipelines',
+  createYourPipeline: 'Créez votre pipeline hop.',
+  name: 'Nom',
+  description: 'Description',
+  createPipeline: 'Créer un pipeline',
+  view: 'Voir',
+};
+
+export default frTranslation;
