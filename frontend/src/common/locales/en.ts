b6d4d95e17d199585048857671dcb2f5f12a5587
Lindanajar <Lindanajar@github.com>
2023-09-17 14:33:47 +0200

    lang files

---
+++ frontend/src/common/locales/en.ts
@@ -0,0 +1,24 @@
+const enTranslation = {
+  appAccounts: 'App Accounts',
+  viewAndManage: 'View and manage settings related to app users.',
+  newUser: 'New User',
+  fullName: 'Full Name',
+  username: 'Username',
+  email: 'Email',
+  phone: 'Phone',
+  gender: 'Gender',
+  country: 'Country',
+  emailVerified: 'Email Verified',
+  status: 'Status',
+  viewDetails: 'View Details',
+  disableUser: 'Disable User',
+  deleteUserConfirmation: 'This user will be denied access, continue?',
+  myPipelines: 'My Pipelines',
+  createYourPipeline: 'Create your hop pipeline.',
+  name: 'Name',
+  description: 'Description',
+  createPipeline: 'Create Pipeline',
+  view: 'View',
+};
+
+export default enTranslation;
