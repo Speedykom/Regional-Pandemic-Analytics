a48362a3d258b585a59fb37ac5fb988058908236
IkbelTalebHssan <t.ikbel@hexastack.com>
2023-09-20 09:44:00 +0100

    Add popup reminder to save changes in Hop ui

--- frontend/src/pages/pipelines/[name].tsx
+++ frontend/src/pages/pipelines/[name].tsx
@@ -3,11 +3,23 @@ import Layout from '@/common/components/Dashboard/Layout';
 import { usePermission } from '@/common/hooks/use-permission';
 import { HopUI } from '@/modules/pipeline/views/HopUI';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 export default function Pipeline() {
   const router = useRouter();
   const { hasPermission } = usePermission();
   const { name } = router.query as { name: string };
+
+  useEffect(() => {
+    if (name && hasPermission('pipeline:read')) {
+      toast.success('Please make sure you are saving changes in Hop UI a', {
+        position: 'bottom-right',
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [name]);
+
   return (
     <Layout>
       {hasPermission('pipeline:read') && name ? (
