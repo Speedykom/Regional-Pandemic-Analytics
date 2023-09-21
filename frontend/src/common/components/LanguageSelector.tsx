bc6b56f6d0ae94e31d3a4e776ae824a0c6c07f5a
Lindanajar <Lindanajar@github.com>
2023-09-22 00:59:04 +0200

    improving languageSelector

--- frontend/src/common/components/LanguageSelector.tsx
+++ frontend/src/common/components/LanguageSelector.tsx
@@ -1,28 +1,25 @@
-import React, { useEffect } from 'react';
-
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const LanguageSelector: React.FC = () => {
   const { i18n } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState(
+    localStorage.getItem('language') || 'en'
+  );
 
   const changeLanguage = (lng: string) => {
+    // Update the language using i18n and also set it in local storage
     i18n.changeLanguage(lng);
-    localStorage.setItem('selectedLanguage', lng);
+    setCurrentLanguage(lng);
+    localStorage.setItem('language', lng);
   };
-
-  useEffect(() => {
-    const storedLanguage = localStorage.getItem('selectedLanguage');
-    if (storedLanguage) {
-      i18n.changeLanguage(storedLanguage);
-    }
-  }, [i18n]);
 
   return (
     <div className="language-selector flex items-center">
       <select
         className="border rounded-md px-2 py-1 focus:outline-none focus:ring focus:border-blue-300 mr-2"
         onChange={(e) => changeLanguage(e.target.value)}
-        value={i18n.language}
+        value={currentLanguage}
       >
         <option value="en">English</option>
         <option value="fr">French</option>
@@ -30,4 +27,5 @@ const LanguageSelector: React.FC = () => {
     </div>
   );
 };
+
 export default LanguageSelector;
