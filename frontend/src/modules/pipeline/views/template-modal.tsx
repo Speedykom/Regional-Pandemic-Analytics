a157227fa4bad75f31b0c717f181e16a3e20f29b
IkbelTalebHssan <t.ikbel@hexastack.com>
2023-09-26 17:35:48 +0100

    feat: searchbar in pipeline templates

--- frontend/src/modules/pipeline/views/template-modal.tsx
+++ frontend/src/modules/pipeline/views/template-modal.tsx
@@ -12,7 +12,8 @@ type TemplateModalProps = {
 export const TemplateModal = ({ onSelect, hideModal }: TemplateModalProps) => {
   const { data: templates } = useTemplatesQuery();
   const [selected, setSelected] = useState<Template>();
-  // const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState<string>(''); // Step 1
+
   const getIcon = (name: string) => {
     const icons = [
       'dhis2',
@@ -53,27 +54,38 @@ export const TemplateModal = ({ onSelect, hideModal }: TemplateModalProps) => {
   };
 
   const handleOk = () => {
-    // only continue if the select exist
     if (selected != undefined) {
-      onSelect(selected); // return the select template to the process chain
+      onSelect(selected);
       setSelected(undefined);
       hideModal();
     }
   };
 
-  const handleCancle = () => {
+  const handleCancel = () => {
     onSelect(false);
     setSelected(undefined);
     hideModal();
   };
+
+  const filteredTemplates = (templates?.data || []).filter(
+    (template) =>
+      template?.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div className="border-t">
       <p className="bg-yellow-200 px-3 py-2 rounded-md mt-3 text-gray-500 w-full">
         Note: select a template and press continue
       </p>
+      <input
+        type="text"
+        placeholder="Search for templates..."
+        className="w-full border border-gray-300 rounded-md p-2 mt-3"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-3">
-        {(templates?.data || []).map((template, index) => (
+        {filteredTemplates.map((template, index) => (
           <div key={index} className="">
             <Tooltip position="top" fontSize="16px">
               <Card
@@ -107,7 +119,7 @@ export const TemplateModal = ({ onSelect, hideModal }: TemplateModalProps) => {
         <Button
           type="button"
           className=" bg-blue-100 px-4 py-2 text-sm text-blue-900 hover:bg-blue-200 border-0"
-          onClick={handleCancle}
+          onClick={handleCancel}
         >
           Cancel
         </Button>
