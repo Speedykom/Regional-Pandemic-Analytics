98918f080a9ea47f8c4178dbfca24a8195705f6a
Lindanajar <Lindanajar@github.com>
2023-09-29 11:02:21 +0200

    rtl , ltr config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -11,10 +11,21 @@ import { AuthProvider } from '@/common/hooks/use-auth';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../i18n';
 
-//const isRtl = localStorage.getItem('language') === 'ar';
 function CsrApp({ Component, pageProps }: AppProps) {
-  const isRtl =
-    typeof window !== 'undefined' && localStorage.getItem('language') === 'ar';
+  // Lisez la langue sélectionnée depuis le localStorage
+  const selectedLanguage =
+    typeof window !== 'undefined' && localStorage.getItem('language');
+
+  // Définissez la direction du texte (RTL) en fonction de la langue sélectionnée
+  if (selectedLanguage === 'ar') {
+    document.body.classList.add('rtl');
+  } else {
+    document.body.classList.remove('rtl');
+  }
+
+  // Initialisez la langue de l'application avec la langue sélectionnée ou par défaut
+  const languageToUse = selectedLanguage || 'en';
+  i18n.changeLanguage(languageToUse);
 
   return (
     <Provider store={store}>
@@ -30,9 +41,7 @@ function CsrApp({ Component, pageProps }: AppProps) {
             }}
           >
             <I18nextProvider i18n={i18n}>
-              <body className={isRtl ? 'rtl' : ''}>
-                <Component {...pageProps} />
-              </body>
+              <Component {...pageProps} />
             </I18nextProvider>
             <ToastContainer />
           </ConfigProvider>
@@ -42,13 +51,7 @@ function CsrApp({ Component, pageProps }: AppProps) {
   );
 }
 
-// We are disabling the SSR for the whole app since the rendering
-// depends mainly on the current user status (logged in or not)
-// Since the user token is stored in the browser local storage, the CSR / SSR rendering
-// will be different.
-//
-// See more info here: https://nextjs.org/docs/messages/react-hydration-error
-
+// Désactivez le rendu côté serveur (SSR) pour toute l'application
 const App = dynamic(async () => CsrApp, { ssr: false });
 
 export default App;
