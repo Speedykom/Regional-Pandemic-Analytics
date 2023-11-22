2d66197f239aeec4ca65845b4fe913f39b376541
AhmAmri <ahmed.amri@speedykom.com>
2023-11-22 19:49:23 +0100

    filter home to only show favorite dashboards

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,6 +1,15 @@
 import { useEffect, useRef, useState } from 'react';
-import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@tremor/react';
-import * as DummyDashboards from '../../modules/superset/views/DummyDashboards.json';
+import {
+  Icon,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from '@tremor/react';
+import { StarIcon } from '@heroicons/react/24/solid';
+
+// import * as DummyDashboards from '../../modules/superset/views/DummyDashboards.json';
 import { embedDashboard } from '@superset-ui/embedded-sdk';
 
 import Layout from '@/common/components/Dashboard/Layout';
@@ -8,96 +17,175 @@ import {
   useEnableDashboardMutation,
   useGenerateGuestTokenMutation,
   useGetDashboardsQuery,
+  useGetFavoriteDashboardsQuery,
 } from '@/modules/superset/superset';
 import { Unauthorized } from '@/common/components/common/unauth';
 import { usePermission } from '@/common/hooks/use-permission';
 import getConfig from 'next/config';
+import {
+  DashboardListResult,
+  FavoriteDashboardResult,
+} from '@/modules/superset/interface';
+
+type DashboardTabProps = {
+  dashboard: DashboardListResult | null;
+  onClick: (dashboardId: string) => void;
+  isSelected: boolean;
+};
+
+type EmbeddedDashboardProps = {
+  selectedDashboard: string | null;
+};
+
+const DashboardTab: React.FC<DashboardTabProps> = ({
+  dashboard,
+  onClick,
+  isSelected,
+}) => (
+  <Tab
+    key={dashboard?.id}
+    onClick={() => onClick(String(dashboard?.id) || '')}
+    defaultChecked={isSelected}
+  >
+    <Icon color="yellow" size="md" icon={StarIcon}></Icon>
+    {dashboard?.dashboard_title}
+  </Tab>
+);
+
+const EmbeddedDashboard: React.FC<EmbeddedDashboardProps> = ({
+  selectedDashboard,
+}) => {
+  const [enableDashboard] = useEnableDashboardMutation();
+  const [generateGuestToken] = useGenerateGuestTokenMutation();
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const embedDash = async () => {
+      if (!selectedDashboard) {
+        return;
+      }
+
+      const response = await enableDashboard(selectedDashboard);
+
+      if (ref.current && response && 'data' in response) {
+        const { uuid } = response.data.result;
+        await embedDashboard({
+          id: uuid,
+          supersetDomain: `${publicRuntimeConfig.NEXT_PUBLIC_SUPERSET_URL}`,
+          mountPoint: ref.current,
+          fetchGuestToken: async () => {
+            const res = await generateGuestToken(uuid);
+            return 'data' in res ? res.data.token : '';
+          },
+          dashboardUiConfig: {
+            hideTitle: true,
+            hideTab: true,
+            filters: {
+              expanded: true,
+              visible: true,
+            },
+          },
+        });
+      }
+    };
+
+    embedDash();
+  }, [selectedDashboard]);
+
+  return (
+    <TabPanel>
+      {selectedDashboard && (
+        <div ref={ref} className="h-screen embed-iframe-container" />
+      )}
+    </TabPanel>
+  );
+};
 
 const { publicRuntimeConfig } = getConfig();
 
 export default function Home() {
-  const [enableDashboard] = useEnableDashboardMutation();
-  const [generateGuestToken] = useGenerateGuestTokenMutation();
   const { hasPermission } = usePermission();
-  const ref = useRef(null);
 
-  var { data } = useGetDashboardsQuery('');
+  var { data, isError, isLoading } = useGetDashboardsQuery('');
+  const dashboardIds = data?.result.map((dashboard: any) =>
+    Number(dashboard?.id)
+  ) || [0];
+
+  var { data: favoriteStatus } = useGetFavoriteDashboardsQuery(dashboardIds);
+
+  // Show only favorite Dashboards
+  if (data && favoriteStatus) {
+    // Extract the IDs of favorite dashboards
+    const favoriteDashboardIds = favoriteStatus.result
+      .filter((favorite: FavoriteDashboardResult) => favorite.value)
+      .map((favorite: FavoriteDashboardResult) => Number(favorite.id));
+
+    // Filter data.result to include only favorite dashboards
+    data = {
+      ...data,
+      result: data.result.filter((dashboard: DashboardListResult) =>
+        favoriteDashboardIds.includes(Number(dashboard.id))
+      ),
+    };
+  }
 
   const [selectedDashboard, setSelectedDashboard] = useState<string | null>(
-    null
+    data?.result.length > 0 ? data?.result[0]?.id : null
   );
 
-  data = DummyDashboards;
+  // data = DummyDashboards;
 
   const handleTabClick = (dashboardId: string) => {
     setSelectedDashboard(dashboardId);
   };
 
-  const embedDash = async () => {
-    if (!selectedDashboard) {
-      return;
-    }
-    const response = await enableDashboard(selectedDashboard);
-    if (ref.current && response && 'data' in response) {
-      const { uuid } = response.data.result;
-      await embedDashboard({
-        id: uuid, // given by the Superset embedding UI
-        supersetDomain: `${publicRuntimeConfig.NEXT_PUBLIC_SUPERSET_URL}`,
-        mountPoint: ref.current, // html element in which iframe render
-        fetchGuestToken: async () => {
-          const res = await generateGuestToken(uuid);
-          return 'data' in res ? res.data.token : '';
-        },
-        dashboardUiConfig: {
-          hideTitle: true,
-          hideTab: true,
-          filters: {
-            expanded: true,
-            visible: true,
-          },
-        },
-      });
-    }
-  };
-
   useEffect(() => {
-    if (selectedDashboard) {
-      setSelectedDashboard(selectedDashboard);
+    if (data?.result.length > 0) {
+      handleTabClick(data.result[0].id);
     }
-    embedDash();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedDashboard]);
+  }, [data]);
 
   if (!hasPermission('dashboard:read')) {
     return <Unauthorized />;
   }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading favorite dashboards</div>;
+  }
   return (
     <Layout>
-      <>
-        <TabGroup>
-          <TabList className="mt-8">
-            {data?.result.map((dashboard: any) => (
-              <Tab
-                key={dashboard?.id}
-                defaultValue={dashboard?.id}
-                onClick={() => handleTabClick(dashboard.id)}
-              >
-                {dashboard?.dashboard_title}
-              </Tab>
-            ))}
-          </TabList>
-          <TabPanels>
-            {data?.result.map((dashboard: any) => (
-              <TabPanel key={dashboard.id}>
-                {dashboard?.id === selectedDashboard && (
-                  <div ref={ref} className="h-screen embed-iframe-container" />
-                )}
-              </TabPanel>
-            ))}
-          </TabPanels>
-        </TabGroup>
-      </>
+      <nav className="mb-5">
+        <div>
+          <h2 className="text-3xl">Favorite Dashboards</h2>
+          <p className="mt-2 text-gray-600">
+            User specific favorite dashboards
+          </p>
+        </div>
+      </nav>
+      <TabGroup className="m-0">
+        <TabList className="m-0" color="emerald" variant="solid">
+          {data?.result.map((dashboard: any) => (
+            <DashboardTab
+              key={dashboard?.id}
+              dashboard={dashboard}
+              onClick={handleTabClick}
+              isSelected={dashboard.id === selectedDashboard}
+            ></DashboardTab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {data?.result.map((dashboard: DashboardListResult) => (
+            <EmbeddedDashboard
+              key={dashboard.id}
+              selectedDashboard={selectedDashboard}
+            />
+          ))}
+        </TabPanels>
+      </TabGroup>
     </Layout>
   );
 }
