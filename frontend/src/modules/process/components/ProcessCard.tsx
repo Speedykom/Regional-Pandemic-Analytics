13a4235f03017ba9a25dc1e248f8e836c92f5951
AhmAmri <ahmed.amri@speedykom.com>
2024-01-25 00:00:42 +0100

    beautify the process chain row

--- frontend/src/modules/process/components/ProcessCard.tsx
+++ frontend/src/modules/process/components/ProcessCard.tsx
@@ -34,12 +34,12 @@ export default function ProcessCard({
   const dateProcess = new Date(process.start_date);
 
   return (
-    <div>
+    <div className="mb-2">
       <Accordion defaultOpen={true}>
         <Disclosure.Button
           disabled={true}
           as="div"
-          className="w-full flex items-center justify-between text-tremor-content-emphasis pr-9 m-3"
+          className="w-full flex items-center justify-between text-tremor-content-emphasis pr-9 rounded-lg"
         >
           <div className="w-fulL flex justify-between items-center">
             <span className="flex space-x-10 ml-3">
@@ -74,7 +74,7 @@ export default function ProcessCard({
                 )}
               </span>
             </span>
-            <span className="flex space-x-3 p-3 mr-7">
+            <span className="flex space-x-3 p-3 mt-4">
               {!process.status && (
                 <Button
                   variant="secondary"
@@ -114,7 +114,8 @@ export default function ProcessCard({
                 </Button>
               )}
             </span>
-
+          </div>
+          <div>
             <ChevronRightIcon
               className={
                 open ? 'ui-open:rotate-90 transform w-4' : 'transform w-4'
