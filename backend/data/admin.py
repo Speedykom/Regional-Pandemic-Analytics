00c512aa9d9d359cc8a9696d3c752c60deb993d6
AhmAmri <ahmed.amri@speedykom.com>
2024-03-06 21:22:16 +0100

    initial cleanup

--- backend/data/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import FileUpload
-
-
-@admin.register(FileUpload)
-class FileUploadAdmin(admin.ModelAdmin):
-    list_display = ("username", "file_name", "file", "date_added")
--- frontend/src/modules/data/interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Data {
-  id: number;
-  username: string;
-  file_name: string;
-  file_type: string;
-  file: string;
-  date_added: string;
-}
