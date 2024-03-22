b94f88d50b40a764be5e4f2305da3d1107ea8091
chaimaraach <chaima.raach@speedykom.de>
2024-03-22 11:19:31 +0100

    Implemented a scrollable table view to display process chain information clearly

--- frontend/src/modules/process/components/StepperElements/Orchestration.tsx
+++ frontend/src/modules/process/components/StepperElements/Orchestration.tsx
@@ -20,43 +20,52 @@ export default function Orchestration({
   const isValidDate = (date: Date) => {
     return date.getTime() !== new Date(0).getTime();
   };
+  console.log({
+    dagId,
+    description,
+    lastParsedTime,
+    nextDagRun,
+  });
 
   return (
     <div className="flex flex-col space-y-4 p-4 max-w-4xl mx-auto bg-white rounded-lg shadow">
       <div className="text-l font-bold text-gray-800 text-center">
         Process Chain Summary
       </div>
-      <table className="w-full border-collapse">
-        <tbody>
-          <tr>
-            <td className="p-3 font-bold bg-prim text-white w-1/4 border border-gray-200">
-              Description
-            </td>
-            <td className="p-3 bg-gray-100 w-3/4 border border-gray-200">
-              {description}
-            </td>
-          </tr>
-          <tr>
-            <td className="p-3 font-bold bg-prim text-white w-1/4 border border-gray-200">
-              Last update
-            </td>
-            <td className="p-3 bg-gray-100 w-3/4 border border-gray-200">
-              {lastParsedTimeDate.toUTCString()}
-            </td>
-          </tr>
-          {isValidDate(nextDagRunDate) && (
+      <div className="overflow-auto" style={{ maxHeight: '100px' }}>
+        <table className="w-full border-collapse">
+          <tbody>
             <tr>
               <td className="p-3 font-bold bg-prim text-white w-1/4 border border-gray-200">
-                Next scheduled execution
+                Description
               </td>
               <td className="p-3 bg-gray-100 w-3/4 border border-gray-200">
-                {nextDagRunDate.toUTCString()}
+                {description}
               </td>
             </tr>
-          )}
-        </tbody>
-      </table>
-      <div className="">
+            <tr>
+              <td className="p-3 font-bold bg-prim text-white w-1/4 border border-gray-200">
+                Last update
+              </td>
+              <td className="p-3 bg-gray-100 w-3/4 border border-gray-200">
+                {lastParsedTimeDate.toUTCString()}
+              </td>
+            </tr>
+            {isValidDate(nextDagRunDate) && (
+              <tr>
+                <td className="p-3 font-bold bg-prim text-white w-1/4 border border-gray-200">
+                  Next scheduled execution
+                </td>
+                <td className="p-3 bg-gray-100 w-3/4 border border-gray-200">
+                  {nextDagRunDate.toUTCString()}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+
+      <div>
         <Divider />
       </div>
       <div>
