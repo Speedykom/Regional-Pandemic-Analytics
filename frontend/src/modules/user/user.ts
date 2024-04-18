be579fdad2064b3f4c60ba58cbacbc1e21ef7ff7
chaimaraach <chaima.raach@speedykom.de>
2024-04-18 21:03:28 +0100

    fix/ added invalidates tags

--- frontend/src/modules/user/user.ts
+++ frontend/src/modules/user/user.ts
@@ -1,4 +1,3 @@
-// Need to use the React-specific entry point to import createApi
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { baseQuery } from '@/common/redux/api';
 import {
@@ -25,20 +24,22 @@ interface ChangePasswordResponse {
 export const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery,
+  tagTypes: ['User'],
   endpoints: (builder) => ({
     getUsers: builder.query<Users, void>({
       query: () => 'account/users',
+      providesTags: ['User'],
     }),
     getUser: builder.query<User, string>({
       query: (id) => `account/user/${id}`,
+      providesTags: (result, error, id) => [{ type: 'User', id }],
     }),
     disableUser: builder.mutation<DisableResponse, string>({
-      query: (id) => {
-        return {
-          url: `account/user/${id}/delete`,
-          method: 'DELETE',
-        };
-      },
+      query: (id) => ({
+        url: `account/user/${id}/delete`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, id) => [{ type: 'User', id }],
     }),
     addUser: builder.mutation<UserResponse, SerialUser>({
       query: (body) => ({
@@ -46,6 +47,7 @@ export const userApi = createApi({
         method: 'POST',
         body,
       }),
+      invalidatesTags: ['User'],
     }),
     modifyUser: builder.mutation<UserResponse, { id: string; userData: any }>({
       query: ({ id, userData }) => ({
@@ -56,6 +58,19 @@ export const userApi = createApi({
           'Content-Type': 'application/json',
         },
       }),
+      onQueryStarted: async (arg, { dispatch, queryFulfilled }) => {
+        const patchResult = dispatch(
+          userApi.util.updateQueryData('getUser', arg.id, (draft) => {
+            Object.assign(draft, arg.userData);
+          })
+        );
+        try {
+          await queryFulfilled;
+          dispatch(userApi.util.invalidateTags([{ type: 'User', id: arg.id }]));
+        } catch {
+          patchResult.undo();
+        }
+      },
     }),
     resetPassword: builder.mutation<{ message: string }, ResetRequest>({
       query: (body) => ({
