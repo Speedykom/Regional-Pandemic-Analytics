5a9a3b8af14bcbfa679ef91928dda88e1de1ba46
OnsKoukiPro <ons.kouki@yahoo.com>
2024-05-02 17:53:18 +0100

    fix/lint issue

--- frontend/src/modules/process/views/add.tsx
+++ frontend/src/modules/process/views/add.tsx
@@ -146,7 +146,7 @@ export const AddProcess = ({
               name="date"
               control={control}
               render={({ field }) => {
-                const { value: dateValue, ...rest } = field;
+                const {...rest } = field;
                 return (
                   <DatePicker
                     {...rest}
