9664452cd1419b725fd18f3a505151b5f171f47f
chaimaraach <chaima.raach@speedykom.de>
2024-05-24 18:23:47 +0100

    fix/ changed default color in tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -30,10 +30,10 @@ module.exports = {
         // light mode
         tremor: {
           brand: {
-            faint: '#007b38', //
+            faint: '#1d4ed8', //
             muted: '#bfdbfe', // blue-200
             subtle: '#60a5fa', // blue-400
-            DEFAULT: '#007b38', //
+            DEFAULT: '#1d4ed8', // blue-200
             emphasis: '#1d4ed8', // blue-700
             inverted: '#ffffff', // white
           },
