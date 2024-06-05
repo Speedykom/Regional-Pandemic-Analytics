6c6a518b5518cece660f02e11c7b68a4abc96eeb
ochaouachi <oussama.chaouachi@speedykom.de>
2024-06-05 18:16:20 +0100

    Adding Swagger under {backendDomain}/swagger

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from api import views
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='RePan API')
+
 
 urlpatterns = [
     path("api/", include("api.urls")),
     path("admin/", admin.site.urls),
+    # For swagger
+    path('swagger/', schema_view),
 ]
 
 if settings.DEBUG:
