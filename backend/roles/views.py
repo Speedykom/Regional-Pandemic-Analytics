6de94ce19d5f08b7ad4b837689e2107329ab9b9d
ochaouachi <oussama.chaouachi@speedykom.de>
2024-06-08 17:35:28 +0100

    Add required Params to endpoints

--- backend/roles/views.py
+++ backend/roles/views.py
@@ -26,6 +26,10 @@ class RoleApiView(APIView):
     API view to update Keycloak client role
     """
     def put(self, request, *args, **kwargs):
+        """
+        name -- string -- required
+        description -- string -- required
+        """
         form_data = {
             "name": request.data.get("name", None),
             "description": request.data.get("description", None),
