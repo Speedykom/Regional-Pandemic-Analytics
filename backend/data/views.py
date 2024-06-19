5142bfbc6fef781f22f72b1d124f527b48a3564e
chaimaraach <chaima.raach@speedykom.de>
2024-06-19 16:09:24 +0100

    added logger to data and hop views

--- backend/data/views.py
+++ backend/data/views.py
@@ -6,6 +6,7 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.parsers import MultiPartParser
 from data.serializers import FileUploadSerializer, FileListSerializer
 from data.models import FileUpload
+import logging
 
 class DataUploadAPIView(APIView):
     keycloak_scopes = {
@@ -13,6 +14,14 @@ class DataUploadAPIView(APIView):
         'POST': 'data:add',
     }
     parser = [MultiPartParser]
+    # Set up logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler('/var/log/backend/backend.log')
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
     @swagger_auto_schema(request_body=openapi.Schema(
         type=openapi.TYPE_OBJECT,
@@ -26,20 +35,29 @@ class DataUploadAPIView(APIView):
         """Retrieves and returns file uploads associated with a specific username"""
         username = request.query_params.get("username")
 
-        data = FileUpload.objects.filter(username=username).order_by("-date_added")
-        serializer = FileUploadSerializer(data, many=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        try:
+            data = FileUpload.objects.filter(username=username).order_by("-date_added")
+            serializer = FileUploadSerializer(data, many=True)
+            self.logger.info("Retrieved file uploads for username: %s", username)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as err:
+            self.logger.error("Error retrieving file uploads for username: %s, Error: %s", username, str(err))
+            return Response({"message": "Error retrieving file uploads"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def post(self, request, *args, **kwargs):
-        """Uplaod a file for a user"""
+        """Upload a file for a user"""
         username = request.data.get('username', None)
 
         if username is None:
+            self.logger.warning("Username not found in the request data")
             return Response({"message": "Username not found"}, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer = FileListSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-
-        return Response({'message': 'File uploaded successfully'}, status=status.HTTP_201_CREATED)
+        try:
+            serializer = FileListSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                self.logger.info("File uploaded successfully for username: %s", username)
+                return Response({'message': 'File uploaded successfully'}, status=status.HTTP_201_CREATED)
+        except Exception as err:
+            self.logger.error("Error uploading file for username: %s, Error: %s", username, str(err))
+            return Response({'message': 'Error uploading file'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
