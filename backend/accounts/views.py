3f114cc9a4b6af18b91df5c382b1fe6411184ace
chaimaraach <chaima.raach@speedykom.de>
2024-07-05 13:46:57 +0100

    added logic to view to differenciate between user and admin actions

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -168,22 +168,30 @@ class UserDetailView(APIView):
         """
         Endpoint for updating a user 
         """
-        user_data = request.data  
+        user_data = request.data
+        current_user_id = get_current_user_id(request)
+        user_id = kwargs['id']
 
         try:
             keycloak_admin = get_keycloak_admin()
-            user_id = kwargs['id']
 
+            # Admin specific actions
             # Check if 'enabled' is in request data to determine enabling user
             if 'enabled' in user_data:
-                user = keycloak_admin.get_user(user_id)
-                user['enabled'] = user_data['enabled']
-                keycloak_admin.update_user(user_id, user)
-                return Response({'message': 'User enabled successfully'}, status=status.HTTP_200_OK)
+                if self.has_admin_role(request):
+                    user = keycloak_admin.get_user(user_id)
+                    user['enabled'] = user_data['enabled']
+                    keycloak_admin.update_user(user_id, user)
+                    return Response({'message': 'User enabled successfully'}, status=status.HTTP_200_OK)
+                else:
+                    return Response({'errorMessage': 'You do not have permission to enable/disable this user.'}, status=status.HTTP_403_FORBIDDEN)
 
-            # Otherwise, update user details
-            keycloak_admin.update_user(user_id, user_data)
-            return Response({'message': 'User updated successfully'}, status=status.HTTP_200_OK)
+            # Normal user actions, update user details
+            if current_user_id == user_id:
+                keycloak_admin.update_user(user_id, user_data)
+                return Response({'message': 'User updated successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errorMessage': 'You do not have permission to update this user.'}, status=status.HTTP_403_FORBIDDEN)
         except Exception as err:
             return Response({'errorMessage': 'Unable to update the user. Error: {}'.format(str(err))}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     def delete(self, request, **kwargs):
@@ -212,7 +220,20 @@ class UserDetailView(APIView):
             return Response({'message': 'User enabled successfully'}, status=status.HTTP_200_OK)
         except Exception as err:
             return Response({'errorMessage': 'Unable to enable the user. Error: {}'.format(str(err))}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
+
+    def has_admin_role(self, request):
+        """
+        Check if the current user has admin rights
+        """
+        try:
+            keycloak_admin = get_keycloak_admin()
+            user_id = get_current_user_id(request)
+            client_id = keycloak_admin.get_client_id(settings.KEYCLOAK_CONFIG['KEYCLOAK_CLIENT_ID'])
+            roles = keycloak_admin.get_client_roles_of_user(user_id=user_id, client_id=client_id)
+            admin_roles = ['administrator']  
+            return any(role['name'] in admin_roles for role in roles)
+        except Exception as err:
+            return False
 class UserRolesView(APIView):
     """
     API view to assign roles to users
