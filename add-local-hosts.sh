c5a015666ec5f8c0d66ed6a5d742abaa56ac97af
AhmAmri <ahmed.amri@speedykom.com>
2024-07-20 12:37:17 +0200

    clean up unused containers

--- add-local-hosts.sh
+++ add-local-hosts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-hosts="127.0.0.1 frontend.igad.local backend.igad.local keycloak.igad.local airflow.igad.local console.minio.igad.local minio.igad.local guest.superset.igad.local superset.igad.local druid.igad.local openhim.igad.local console.openhim.igad.local"
+hosts="127.0.0.1 frontend.igad.local backend.igad.local keycloak.igad.local airflow.igad.local console.minio.igad.local minio.igad.local guest.superset.igad.local superset.igad.local druid.igad.local"
 if grep -q "$hosts" /etc/hosts; then
     echo hosts already added!
 else
