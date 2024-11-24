81569c1fb0c5809d6ef678534d143bac1860c3e5
Med Dhia Kassab <dhia.kassab@speedykom.de>
2024-11-24 15:52:12 +0100

    changed LearnMore component content (rebranding to d-OHP)

--- frontend/src/common/components/LearnMore.tsx
+++ frontend/src/common/components/LearnMore.tsx
@@ -5,7 +5,7 @@ const LearnMore: React.FC = () => {
     <div className="m-4 p-4 bg-gray-200 border border-gray-300 rounded">
       <h2 className="text-xl font-bold text-green-600 mb-2">
         {' '}
-        Learn More about RePan
+        Learn More about d-OHP
       </h2>
       <p className="text-base text-gray-700">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit...
