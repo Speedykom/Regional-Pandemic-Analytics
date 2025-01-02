e7cbe192bddb612933485a9e42e8fbfb1fa5ad3f
OnsKoukiPro <ons.koukipro@yahoo.com>
2025-01-02 18:13:30 +0100

    #330 Chore : Keycloak cleanups

--- backend/roles/views.py
+++ backend/roles/views.py
@@ -10,17 +10,22 @@ from django.conf import settings
 #Api to create and list all roles
 class RoleApiView(APIView):
     permission_classes = [AllowAny, ]
-    
+
     """_api view to get client roles_
     """
     def get(self, request, *args, **kwargs):
         """
-        Endpoint for listing to keycloak client role 
+        Endpoint for listing keycloak client roles, excluding default roles.
         """
         try:
             keycloak_admin = get_keycloak_admin()
             realm_roles = keycloak_admin.get_realm_roles()
-            return Response(realm_roles, status=status.HTTP_200_OK)
+
+            # Exclude default roles
+            excluded_roles = {"default-roles-stack", "offline_access", "uma_authorization"}
+            filtered_roles = [role for role in realm_roles if role.get('name') not in excluded_roles]
+
+            return Response(filtered_roles, status=status.HTTP_200_OK)
         except Exception as err:
             return Response({'errorMessage': 'Unable to retrieve the roles'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
@@ -29,7 +34,7 @@ class RoleApiView(APIView):
     """
     def put(self, request, *args, **kwargs):
         """
-        Endpoint for updating keycloak client role 
+        Endpoint for updating keycloak client role
         """
         form_data = {
             "name": request.data.get("name", None),
