918130a171e6ba44a8598aa5baf3ca1d05a3a19b
OnsKoukiPro <ons.koukipro@yahoo.com>
2025-01-02 19:03:08 +0100

    #346 Improve pipeline execution and check_status message

--- backend/pipeline/rules/parquet_file_output_rule.py
+++ backend/pipeline/rules/parquet_file_output_rule.py
@@ -5,7 +5,6 @@ from .rule import Rule
 
 class ParquetFileOutputRule:
     rules = [
-        Rule("filename_base", "ftp://${minio_ftp}/parquets/${user_id}/${dag_display_name}", "InvalidFilenameBase"),
         Rule("filename_ext", "parquet", "InvalidFilenameExtension"),
         Rule("filename_include_copy", "N", "InvalidFilenameIncludeCopy"),
         Rule("filename_include_date", "N", "InvalidFilenameIncludeDate"),
@@ -18,14 +17,17 @@ class ParquetFileOutputRule:
         self.xml_transform_element = xml_transform_element
 
     def is_parquet_transform(self):
-        # First check if the XML transform element is a ParquetFileOutput element 
+        # First check if the XML transform element is a ParquetFileOutput element
         xml_transform_element_type = self.xml_transform_element.find("type")
         return xml_transform_element_type is not None and xml_transform_element_type.text == "ParquetFileOutput"
-    
+
     def is_valid(self):
         valid_pipeline = False
         check_text = "MissingParquetTransform"
 
+        filename_base_element = self.xml_transform_element.find("filename_base")
+        if filename_base_element is None or not filename_base_element.text or not filename_base_element.text.strip():
+            return valid_pipeline, "InvalidFilenameBase"
         for rule in self.rules:
             element = self.xml_transform_element.find(rule.name)
             if element is None or element.text != rule.value:
