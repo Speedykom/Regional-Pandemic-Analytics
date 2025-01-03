614b853ed169bc7d155d4ce9a267c679f1bf3a3f
OnsKoukiPro <ons.koukipro@yahoo.com>
2025-01-03 12:41:14 +0100

    #357 Refactor: Redesign & Simplify Process Chain Features

--- frontend/src/modules/process/views/list.tsx
+++ frontend/src/modules/process/views/list.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
-import { Button } from '@tremor/react';
+import {
+  Button,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+} from '@tremor/react';
 import { Loader } from '@/common/components/Loader';
 import { usePermission } from '@/common/hooks/use-permission';
-import { useGetProcessQuery } from '../process';
+import { useGetProcessQuery, useToggleProcessStatusMutation } from '../process';
 import { DagDetails, DagDetailsResponse } from '../interface';
 import ProcessCard from '../components/ProcessCard';
 import { AddProcess } from './add';
 import { useGetAllPipelinesQuery } from '@/modules/pipeline/pipeline';
 import { useTranslation } from 'react-i18next';
 import { Switch } from '@tremor/react';
+import { FaPlay } from 'react-icons/fa6';
+import { AiOutlinePieChart, AiOutlineStop } from 'react-icons/ai';
+import { TbReportSearch } from 'react-icons/tb';
+import ProcessChainDialog from './dialog';
 
 export default function ProcessChainList() {
   const { hasPermission } = usePermission();
@@ -43,10 +56,12 @@ export default function ProcessChainList() {
 
     var processChainToShowLength = 0;
     if (showDisabled) {
-      processChainToShowLength = processChainList?.dags?.length;
+      processChainToShowLength = processChainList?.dags?.filter(
+        (e) => e.status === false
+      ).length;
     } else {
       processChainToShowLength = processChainList.dags.filter(
-        (dag) => dag.status == false
+        (dag) => dag.status == true
       ).length;
     }
 
@@ -57,33 +72,36 @@ export default function ProcessChainList() {
       processChainToShowLength
     );
     return (
-      <div className="flex justify-end items-center mt-4">
-        <div className="mr-4">
-          {t('showing')} {startItem} – {endItem} {t('of')}{' '}
-          {processChainToShowLength}
+      <>
+        <div className="flex justify-end items-center mt-4">
+          <div className="mr-4">
+            {t('showing')} {startItem} – {endItem} {t('of')}{' '}
+            {processChainToShowLength}
+          </div>
+          <div className="flex">
+            <Button
+              className="bg-prim hover:bg-green-900 border-0 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline cursor-pointer mr-2"
+              size="xs"
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage(currentPage - 1)}
+            >
+              &larr; {t('prev')}
+            </Button>
+            <Button
+              className="bg-prim hover:bg-green-900 border-0 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline cursor-pointer"
+              size="xs"
+              disabled={currentPage === totalPages}
+              onClick={() => setCurrentPage(currentPage + 1)}
+            >
+              {t('next')} &rarr;
+            </Button>
+          </div>
         </div>
-        <div className="flex">
-          <Button
-            className="bg-prim hover:bg-green-900 border-0 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline cursor-pointer mr-2"
-            size="xs"
-            disabled={currentPage === 1}
-            onClick={() => setCurrentPage(currentPage - 1)}
-          >
-            &larr; {t('prev')}
-          </Button>
-          <Button
-            className="bg-prim hover:bg-green-900 border-0 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline cursor-pointer"
-            size="xs"
-            disabled={currentPage === totalPages}
-            onClick={() => setCurrentPage(currentPage + 1)}
-          >
-            {t('next')} &rarr;
-          </Button>
-        </div>
-      </div>
+      </>
     );
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const renderProcessChainData = (processChainList: DagDetails[]) => {
     var processChainToShow = null;
     if (!showDisabled) {
@@ -123,9 +141,28 @@ export default function ProcessChainList() {
       });
     }
   };
+  const [tab, setTab] = useState<number>(1);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [processData, setProcessData] = useState<DagDetails | null>(null);
+  const [toggleProcessStatus] = useToggleProcessStatusMutation();
 
+  function handleToggleProcessStatus(
+    event: React.MouseEvent<SVGElement>,
+    dagId: string
+  ) {
+    toggleProcessStatus(dagId);
+  }
   return (
     <div>
+      {processData && (
+        <ProcessChainDialog
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          tab={tab}
+          setTab={setTab}
+          processData={processData}
+        />
+      )}
       <div className="flex justify-between items-center">
         <div>
           <h2 className="text-3xl">{t('processChain')}</h2>
@@ -174,10 +211,128 @@ export default function ProcessChainList() {
           </div>
         )}
         {isSuccess && pipelineList && (
-          <div>
-            {renderProcessChainData(data?.dags)}
-            {renderPagination(data)}
-          </div>
+          <>
+            <Card className="p-0 rounded-md">
+              <Table className="rounded-md">
+                <TableHead className="bg-[#F9FAFB] border-[1px] border-[#E4E7EC]">
+                  <TableHeaderCell className="text-[#475467]">
+                    {t('processChainDialog.ProcessChainName')}
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-[#475467]">
+                    {t('processChainDialog.dataPip')}
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-[#475467]">
+                    {t('processChainDialog.period')}
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-[#475467]">
+                    {t('processChainDialog.processStatus')}
+                  </TableHeaderCell>
+                  <TableHeaderCell className="text-[#475467]">
+                    {t('processChainDialog.execState')}
+                  </TableHeaderCell>
+                  <TableHeaderCell className="w-1/4 border-[#E4E7EC] border-l-[1px] text-[#475467]">
+                    {t('processChainDialog.actions')}
+                  </TableHeaderCell>
+                </TableHead>
+                <TableBody>
+                  {data?.dags
+                    .filter((e) => (showDisabled ? !e.status : e.status)) // Filter based on status
+                    .map((e, k) => (
+                      <TableRow
+                        key={k}
+                        className="border-[1px] border-[#E4E7EC]"
+                      >
+                        <TableCell className="text-black">{e?.name}</TableCell>
+                        <TableCell className="text-blue-700 underline font-normal">
+                          {e?.data_source_name}
+                        </TableCell>
+                        <TableCell>{e?.schedule_interval}</TableCell>
+                        <TableCell className="my-auto">
+                          {e?.status ? (
+                            <>
+                              <span className="text-2xl text-green-700 relative top-[3.5px]">
+                                •
+                              </span>{' '}
+                              <span className="!font-medium">
+                                {t('processChainDialog.activeStatus')}
+                              </span>
+                            </>
+                          ) : (
+                            <>
+                              <span className="text-2xl text-red-700 relative top-[3.5px]">
+                                •
+                              </span>{' '}
+                              <span className="!font-medium">
+                                {t('processChainDialog.inactiveStatus')}
+                              </span>
+                            </>
+                          )}
+                        </TableCell>
+                        <TableCell>
+                          {e.dataset_success ? (
+                            <Button className="bg-transparent py-3 hover:bg-transparent text-green-700 border-green-700 hover:border-green-700">
+                              {t('processChainDialog.success')}
+                            </Button>
+                          ) : (
+                            <Button className="bg-transparent py-3 hover:bg-transparent text-red-700 border-red-700 hover:border-red-700">
+                              {t('processChainDialog.Failed')}
+                            </Button>
+                          )}
+                        </TableCell>
+                        <TableCell className="border-[#E4E7EC] border-l-[1px]">
+                          <div className="flex flex-row gap-x-2">
+                            <FaPlay
+                              size="40"
+                              color="#15803d"
+                              className={
+                                e?.status == false
+                                  ? `p-2 rounded-md border-[1.8px] border-green-700 cursor-pointer`
+                                  : `p-2 rounded-md border-[1.8px] border-gray-300 bg-gray-100 text-gray-400 pointer-events-none opacity-50`
+                              }
+                              onClick={(event) =>
+                                handleToggleProcessStatus(event, e?.dag_id)
+                              }
+                            />
+                            <AiOutlineStop
+                              size="40"
+                              color="#b91c1c"
+                              className={
+                                e?.status == true
+                                  ? `p-2 rounded-md border-[1.8px] border-red-700 cursor-pointer`
+                                  : `p-2 rounded-md border-[1.8px] border-gray-300 bg-gray-100 text-gray-400 pointer-events-none opacity-50`
+                              }
+                              onClick={(event) =>
+                                handleToggleProcessStatus(event, e?.dag_id)
+                              }
+                            />
+                            <AiOutlinePieChart
+                              size="40"
+                              color="black"
+                              className="p-2 rounded-md border-[1.8px] border-black cursor-pointer"
+                              onClick={() => {
+                                setIsOpen(true);
+                                setTab(3);
+                              }}
+                            />
+                            <TbReportSearch
+                              size="40"
+                              color="black"
+                              className="p-2 rounded-md border-[1.8px] border-black cursor-pointer"
+                              onClick={() => {
+                                setTab(1);
+                                setIsOpen(true);
+                                setProcessData(e ?? null);
+                              }}
+                            />
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+              <div className="py-1">{renderPagination(data)}</div>
+            </Card>
+          </>
         )}
       </div>
       {addComponent && isSuccessPipeline && (
