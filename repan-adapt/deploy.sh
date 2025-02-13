c2cbd49f66ad189357fa485aaadd3b13ae502803
ochaouachi <oussama.chaouachi@speedykom.de>
2025-02-13 11:35:24 +0100

    fixing variable

--- repan-adapt/deploy.sh
+++ repan-adapt/deploy.sh
@@ -23,7 +23,7 @@ echo "Adapting local domains complete."
 # Adapting dev domains
 echo "Adapting dev domains."
 old_dev_domain="igad-health.eu"
-new_dev_domain=$local_domain
+new_dev_domain=$dev_domain
 #find all old domains and change them with new domain
 find "$project_directory" -type f -exec sed -i "s/$old_dev_domain/$new_dev_domain/g" {} +
 echo "Adapting dev domains complete."
@@ -31,7 +31,7 @@ echo "Adapting dev domains complete."
 # Adapting prod domains
 echo "Adapting prod domains."
 old_local_domain="prod-igad.domain"
-new_local_domain=$local_domain
+new_local_domain=$prod_domain
 #find all old domains and change them with new domain
 find "$project_directory" -type f -exec sed -i "s/$old_dev_domain/$new_local_domain/g" {} +
 echo "Adapting prod domain complete."
